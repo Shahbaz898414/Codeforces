5a0d530871501ad90efc016f060f472d6a78bccb
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-07 18:49:21 +0530

    1

--- Practice Question/800-2000/C. Restore the Array.cpp
+++ Practice Question/800-2000/C. Restore the Array.cpp
@@ -3,9 +3,12 @@
 #define ll long long 
 using namespace std;
 
-void solve(){
-    int n;
-    cin >> n;
+
+int main() {
+
+   ll t;cin>>t;
+   while(t--) {
+
     vector<int>b(n-1), a;
     for(int i = 0; i < n - 1; i++) cin >> b[i];
     a.push_back(b[0]);
@@ -15,43 +18,6 @@ void solve(){
     a.push_back(b[n - 2]);
     for(auto &i : a) cout << i << ' ';
     cout << "\n";
-}
-
-
-int main() {
-
-   ll t;cin>>t;
-   while(t--) {
-
-    solve();
-    //  ll n,m,k;cin>>n;
-    //  vector<ll> v(n),arr;
-
-    //  for(int i=0;i<n;i++) {
-    //   cin>>v[i];
-    //  }
-
-
-    //  for(int i=1;i<n;i++) {
-    //   if(v[i-1]<v[i]){
-    //     arr.push_back(v[i-1]);
-    //     arr.push_back(0);
-    //   }else if(v[i-1]>v[i]){
-    //     arr.push_back(0);
-    //     arr.push_back(v[i]);
-    //   }
-    //   else {
-    //     arr.push_back(v[i]);
-    //   }
-    //  }
-
-    //  for(auto it:arr) {
-    //   cout<<it<<" ";
-    //  }
-
-    //  cout<<endl;
-    
-    
 
 
     
