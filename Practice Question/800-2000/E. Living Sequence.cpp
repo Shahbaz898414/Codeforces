90160c7f71fefbb24d85015e2748e368893bd2c6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-07 16:34:37 +0530

    1

--- Practice Question/800-2000/E. Living Sequence.cpp
+++ Practice Question/800-2000/E. Living Sequence.cpp
@@ -22,11 +22,11 @@ int main() {
         }
         // cout<<endl;
         reverse(v.begin(),v.end());
-        for(auto it: v){
-          cout<<it<<" ";
-        }
+        // for(auto it: v){
+        //   cout<<it<<" ";
+        // }
 
-        cout<<endl;
+        // cout<<endl;
 
         for(int i=0;i<v.size();i++) {
           cout<<(char)(v[i]<4 ?(v[i]+'0'):(v[i]+'1'));
