339f0b5361a084dfd1a28acf2017f436a49e69ee
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-07 16:23:37 +0530

    1

--- Practice Question/800-2000/E. Living Sequence.cpp
+++ Practice Question/800-2000/E. Living Sequence.cpp
@@ -10,19 +10,25 @@ int main() {
    while(t--) {
      ll n,m;cin>>n;
      vector<ll> v;
-
+      cout<<n<<endl;
      if(n<=3)  cout<<n<<endl;
      else {
       
         while(n>0){
           v.push_back(n%9);
+          cout<<n%9<<" ";
           n/=9;
         }
-
+        cout<<endl;
         reverse(v.begin(),v.end());
+        // for(auto it: v){
+        //   cout<<it<<" ";
+        // }
+
+        // cout<<endl;
 
         for(int i=0;i<v.size();i++) {
-          cout<<(char)(v[i]<4?(v[i]+'0'):(v[i]+'1'));
+          cout<<(char)(v[i]<4 ?(v[i]+'0'):(v[i]+'1'));
         }
         cout<<endl;
      }
