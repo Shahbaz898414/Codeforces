6f109985e8a4a9c7ac1dbf6f46ab47031b2ceb19
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-07 18:55:55 +0530

    1

---
+++ Practice Question/800-2000/C. Make It Permutation.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+
+int main() {
+
+   ll t;cin>>t;
+   while(t--) {
+      int n,m,k;cin>>n>>m>>k;
+   
+
+    
+   }
+
+	
+	return 0;
+}
--- Practice Question/800-2000/C. Restore the Array.cpp
+++ Practice Question/800-2000/C. Restore the Array.cpp
@@ -8,13 +8,19 @@ int main() {
 
    ll t;cin>>t;
    while(t--) {
-
+      int n;cin>>n;
     vector<int>b(n-1), a;
     for(int i = 0; i < n - 1; i++) cin >> b[i];
+
+
     a.push_back(b[0]);
+
+
     for(int i = 0; i < n - 2; i++){
         a.push_back(min(b[i], b[i + 1]));
     }
+
+    
     a.push_back(b[n - 2]);
     for(auto &i : a) cout << i << ' ';
     cout << "\n";
