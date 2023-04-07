51659cba547f0f3afb978d697c7b5216ed676320
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-07 18:48:45 +0530

    1

--- Practice Question/800-2000/C. Restore the Array.cpp
+++ Practice Question/800-2000/C. Restore the Array.cpp
@@ -8,11 +8,11 @@ void solve(){
     cin >> n;
     vector<int>b(n-1), a;
     for(int i = 0; i < n - 1; i++) cin >> b[i];
-    a.emplace_back(b[0]);
+    a.push_back(b[0]);
     for(int i = 0; i < n - 2; i++){
-        a.emplace_back(min(b[i], b[i + 1]));
+        a.push_back(min(b[i], b[i + 1]));
     }
-    a.emplace_back(b[n - 2]);
+    a.push_back(b[n - 2]);
     for(auto &i : a) cout << i << ' ';
     cout << "\n";
 }
