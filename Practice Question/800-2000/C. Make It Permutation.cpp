cb017138cbc52076f09f2733a34819fb240e4ada
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-09 00:49:49 +0530

    1

--- Practice Question/800-2000/C. Make It Permutation.cpp
+++ Practice Question/800-2000/C. Make It Permutation.cpp
@@ -9,10 +9,17 @@ int main() {
    ll t;cin>>t;
    while(t--) {
       int n,m,k;cin>>n>>m>>k;
+
+
+      vector<ll>  v(n);
+
+      for(int i=0;i<n;i++) {
+         cin>>v[i];
+      }
    
+
+
     
     
    }
-
-
 }
