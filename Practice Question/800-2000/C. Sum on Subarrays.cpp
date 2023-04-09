aaf0024635cd030ef89ceddba906e7413c18af9f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-09 18:27:56 +0530

    1

---
+++ Practice Question/800-2000/C. Sum on Subarrays.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
+#define fr(i, a, b) for (int i = a; i >= b; i--)
+
+void sort(vector<ll>& a) {
+    sort(a.begin(), a.end());
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll n;ll c, d;cin >> n >> c ;
+       
+    }
+
+    return 0;
+}
