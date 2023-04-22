2770ecfa68ccfd943e12bf1e29a485044414f301
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-22 18:09:21 +0530

    1

--- Practice Question/800-2000/B. Card Constructions.cpp
+++ Practice Question/800-2000/B. Card Constructions.cpp
@@ -46,4 +46,20 @@ int main(){
 	}
 	return 0;
 }
+
+
+
+long long int t,n,m,k; cin>>t;
+while(t--){
+      cin>>n; m=0;
+     while(n>1) {
+          k=2;
+         while(n>=k) {
+          n=n-k; k=k+3;
+        }
+        m++;
+     }
+      cout<<m<<endl;
+}
+
 */
