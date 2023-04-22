2776535c1c06c690adfe6a85a6b03115c92b8aed
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-22 18:31:19 +0530

    1

--- Practice Question/800-2000/B. Card Constructions.cpp
+++ Practice Question/800-2000/B. Card Constructions.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long
@@ -24,7 +24,9 @@ int main() {
       int n,cnt=0;cin>>n;
       for(int i=N-1;i;--i)
       	while(n>=a[i])
-        	n-=a[i],++cnt;
+        	 n-=a[i],++cnt;
+
+
       cout<<cnt<<endl;
 
     }
@@ -33,37 +35,3 @@ int main() {
 
 
 
-/*
-
-#include<cstdio>
-const int N=2e4+4;
-int a[N],n,t,s;
-int main(){
-	for(int i=1;i<N;++i)	a[i]=i*(3*i+1)/2;
-	scanf("%d",&t);
-	while(t--){
-		scanf("%d",&n);s=0;
-		for(int i=N-1;i;--i)	while(n>=a[i])	n-=a[i],++s;
-		printf("%d\n",s);
-	}
-	return 0;
-}
-
-
-
-long long int t,n,m,k; cin>>t;
-while(t--){
-      cin>>n; m=0;
-     while(n>1) {
-          k=2;
-        while(n>=k) {
-          n=n-k; k=k+3;
-        }
-        m++;
-     }
-      cout<<m<<endl;
-}
-
-
-
-*/
