83cc35ab18e49c3b2b19811cc5203ac3a036d5ee
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-22 18:27:58 +0530

    1

--- Practice Question/800-2000/B. Card Constructions.cpp
+++ Practice Question/800-2000/B. Card Constructions.cpp
@@ -17,9 +17,15 @@ int a[N];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    for(int i=1;i<N;++i)	a[i]=i*(3*i+1)/2;
     int t;cin>>t;
     while(t--) {
-      int n;cin>>n;
+      int n,cnt=0;cin>>n;
+
+      for(int i=N-1;i;--i)	while(n>=a[i])	n-=a[i],++cnt;
+
+      cout<<cnt<<endl;
 
     }
     return 0;
