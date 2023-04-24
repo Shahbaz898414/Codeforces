a7815fd45533dec7825e0d4305b8b3327029f3a4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-24 23:35:13 +0530

    1

--- Practice Question/800-2000/B. JoJo's Incredible Adventures.cpp
+++ Practice Question/800-2000/B. JoJo's Incredible Adventures.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 #define f(i, a, b) for (int i = a; i < b; i++)
 #define fe(i, a, b) for (int i = a; i <= b; i++)
@@ -15,17 +14,32 @@ const int N=2e4+4;
 int a[N];
 
 int main() {
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    // for(int i=1;i<N;++i) a[i]=i*(3*i+1)/2;
     int t;cin>>t;
     while(t--) {
-      string s;cin>>s;
+       string s; cin >> s; s += s;
+        int k = 0, z = 0;
+        for (char c : s) {
+            z = c == '1' ? z+1 : 0;
+            k = max(k, z);
+        }
+        const int n = s.size() / 2;
 
+        if (k > n) {
+            cout << (ll)n*n << '\n';
+        } else {
+            const ll side_a = (k+1)/2;
+            const ll side_b = (k+2)/2;
+            cout << side_a * side_b << '\n';
+        }
+
+
+    
+    
+    
     }
+
     return 0;
 }
-
-
-
