6e8ef98eada443dd589d964764a231a71b53f8db
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-26 18:36:02 +0530

    1

---
+++ Practice Question/800-2000/A. Constructive Problem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
+#define fr(i, a, b) for (int i = a; i >= b; i--)
+
+void sort(vector<ll>& a) {
+    sort(a.begin(), a.end());
+}
+
+const int N=2e4+4;
+
+int a[N];
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--) {
+      
+    }
+
+    return 0;
+}
