0107f8cf9561c00efe064fe849914e175465401a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-01 13:19:41 +0530

    1

--- Practice Question/800-2000/D. Super-Permutation.cpp
+++ Practice Question/800-2000/D. Super-Permutation.cpp
@@ -212,7 +212,14 @@ signed main() {
     }else {
       if(n%2)  cout<<-1<<endl;
       else {
-        
+        for (int i = 0; i < n; ++i) {
+                if (i % 2) {
+                    cout << i << " ";
+                } else {
+                    cout << n - i << " ";
+                }
+            }
+            cout << "\n";    
       }
     }
 
