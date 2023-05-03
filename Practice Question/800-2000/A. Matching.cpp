67d443777b31070d669da6b6b95fb561b0c83b85
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-03 18:58:02 +0530

    1

--- Practice Question/800-2000/A. Matching.cpp
+++ Practice Question/800-2000/A. Matching.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define mod 1000000007
@@ -144,10 +144,9 @@ signed main() {
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  ll t; cin >> t;
-  while (t--) {
-    ll n; string s;
-    cin>>s;
+  ll t;cin>>t;
+  while(t--) {
+    string s;cin>>s;
    
     
 
