1044c59232371364d22ba5b7de52af4fa109076d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-12 23:00:01 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -335,20 +335,6 @@ signed main()
         } 
 
 
-        // void rotateMatrix(vector<vector<int>> &v, int n)
-// {
-//   for (int i = 0; i < n / 2; i++)
-//   {
-//     for (int j = i; j < n - i - 1; j++)
-//     {
-//       int ptr = v[i][j];
-//       v[i][j] = v[n - 1 - j][i];
-//       v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
-//       v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
-//       v[j][n - 1 - i] = ptr;
-//     }
-//   }
-// }
 
         cout << ans << endl;  
     
