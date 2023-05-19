f232f189aa227cc73d9b301dba0cd8c2e7e499ab
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-19 15:18:57 +0530

    1

--- Practice Question/800-2000/C. Counting Orders.cpp
+++ Practice Question/800-2000/C. Counting Orders.cpp
@@ -156,21 +156,12 @@ signed main() {
   int t; cin >> t;
   while(t--) {
     ll n;cin>> n;
-    vector<ll> v(n+1);
+     ll ans=0;
+     vector<ll> v1(n+1),v2(n+1);
 
 
-    for (int i = 1; i <=n; i++) {
-      cin>>v[i];
-    }
 
    
-
-    for (int i = 1; i <=n; i++){ 
-      ans=__gcd(ans,abs(v[i]-i));
-    }
-
-
-    cout<<ans<<endl;
     
   }
   return 0;
