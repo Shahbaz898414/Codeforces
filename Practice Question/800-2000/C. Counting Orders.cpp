0661810ae022745e3dbb7ba6cb764d8c5197d676
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-19 16:16:13 +0530

    1

--- Practice Question/800-2000/C. Counting Orders.cpp
+++ Practice Question/800-2000/C. Counting Orders.cpp
@@ -155,9 +155,40 @@ signed main() {
   cin.tie(nullptr);
   int t; cin >> t;
   while(t--) {
-    ll n;cin>> n;
+     ll n;cin>> n;
      ll ans=0;
      vector<ll> v1(n+1),v2(n+1);
+
+     for(ll i = 0; i < n; i++){
+       cin>>v1[i];
+     }
+
+     sort(v1.begin(),v1.end());
+
+     for(ll i = 0; i < n; i++) {
+       cin>>v2[i];
+     }
+
+
+      sort(v2.begin(),v2.end(),greater<>());
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      cout<<v1[i]<<" ";
+    }
+    cout<<endl;
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      cout<<v2[i]<<" ";
+    }
+
+    cout<<endl;
+    
+     
+     
 
 
 
