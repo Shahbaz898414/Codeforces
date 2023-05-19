af1b4543d2db43d64fcdb24ecb8ac70c0be59441
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-19 14:52:45 +0530

    1

--- Practice Question/800-2000/B. Permutation Swap.cpp
+++ Practice Question/800-2000/B. Permutation Swap.cpp
@@ -162,16 +162,15 @@ signed main() {
       cin>>v[i];
     }
 
+    ll ans;
+
     for (int i = 0; i < n; i++){ 
-      if(i+1!=v[i]){
-
-      }
+      ans=__gcd(ans,v[i]-i+1);
     }
-    
-    
 
-        
-   
+
+    cout<<ans<<endl;
+    
   }
   return 0;
 }
