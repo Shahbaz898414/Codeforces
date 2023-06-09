3a0ffb6cf3c79cd298efbdc190069fc78b0d4434
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-09 18:47:41 +0530

    1

--- Practice Question/800-2000/B. Array merging.cpp
+++ Practice Question/800-2000/B. Array merging.cpp
@@ -159,61 +159,50 @@ void dfs(int node,vector<vector<pair<int,int>>> &adj,int par,int ire,int idx){
 
 
 
-signed main()
+int main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+  // ios::sync_with_stdio(false);
+  // cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) {
 
-    ll n;cin>>n;
-
-    ll arr[n];
-    ll b[n];
-    ll ans=0;
-
-    for (ll i = 0; i < n; i++) {
-      cin>>arr[i];
-    }
-
-    for (ll i = 0; i < n; i++) {
-      cin>>b[i];
-    }
-
-    ll cnt1[2*n+1]={0};
-    ll cnt2[2*n+1]={0};
-
-    for (int i = 0; i < n; i++) {
+   ll n;
+  cin>>n;
+  ll a[n];
+  ll b[n];
+  
+  for(ll i=0;i<n;i++)
+  cin>>a[i];
+  
+  for(ll i=0;i<n;i++)
+  cin>>b[i];
+  
+  ll ans=0;
+  
+  ll cnt1[2*n+1]={0};
+  ll cnt2[2*n+1]={0};
+  
+  for(ll i=0;i<n;i++){
       ll j=i;
-      while(i<n and a[j]==a[i]){
-        j++;
-      }
+      while(j<n && a[j]==a[i])
+      j++;
       cnt1[a[i]]=max(cnt1[a[i]],j-i);
-      i=--j; 
-    }
-
-
-
-    for(int i = 0; i < n; i++) {
+      i=--j;
+  }
+  
+  for(ll i=0;i<n;i++){
       ll j=i;
-      while(i<n and b[j]==b[i]){
-        j++;
-      }
+      while(j<n && b[j]==b[i])
+      j++;
       cnt2[b[i]]=max(cnt2[b[i]],j-i);
-      i=--j; 
-    }
-
-
-    for (ll i = 1; i <=2*n; i++)
-    {
-      /* code */
-      ans=max(ans,cnt1[i]+cnt2[i]);
-    }
-
-    cout<<ans<<endl;
-    
-    
+      i=--j;
+  }
+  
+  for(ll i=1;i<=2*n;i++)
+  ans=max(ans,cnt1[i]+cnt2[i]);
+  
+  cout<<ans<<"\n";    
   }
 
   return 0;
