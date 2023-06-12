f5a06012acfe8bcd436399e8769821426cd5f931
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-13 00:05:32 +0530

    1

--- Practice Question/800-2000/B - ABCDEFG.cpp
+++ Practice Question/800-2000/B - ABCDEFG.cpp
@@ -1,50 +1,19 @@
-
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
-int main() {
+int main()
+{
 
   char a, b;
   cin >> a >> b;
 
-  ll s1=a-'a';
-  ll s2=b-'a';
+  ll s1 = a - 'A';
+  ll s2 = b - 'B';
 
-  // cout<<s1<<" "<<s2<<endl;
-   ll arr[7];
+  ll arr[] = {0, 3, 4, 8, 9, 14, 23};
 
-   arr[1]=3;
-   arr[2]=1;
-   arr[3]=4;
-   arr[4]=1;
-   arr[5]=5;
-   arr[6]=9;
-
-   for (int i = 1; i <=6; i++)
-   {
-    /* code */
-    arr[i]=arr[i]+arr[i-1];
-   }
-
-
-   cout<<abs(arr[min(s1,s2)]-arr[max(s1,s2)])<<endl;
-
-
-   
-
-
-  ll ab = 1;
-  ll bc = 1;
-  ll cd = 4;
-  ll de = 1;
-  ll ef = 5;
-  ll fg = 9;
-
-
-
+  cout << abs(arr[s2] - arr[s1]) << endl;
 
   return 0;
 }
-
-
