46c397c14c4d9f8622e80f17694ef3f0711a781f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-14 21:05:28 +0530

    1

--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -22,52 +22,63 @@ const int MOD = 1000000007;
 //     return false; // No subset found with bitwise AND equal to B
 // }
 
-void check_bitwise_AND() {
-     int n, k;
-    cin >> n >> k;
-    string s;
-    cin >> s;
+void check_bitwise_AND()
+{
+  int n, k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
 
-    int ans = 1;
-    int count = 0;
-    bool start = false;
-    int count2 = 0;
-    bool check = false;
+  int main = 1;
+  int cnt = 0;
+  bool st = false;
+  int vnb = 0;
+  bool fl = false;
 
-    for (int i = 0; i < n; i++) {
-        if (s[i] == 'a' || s[i] == 'i' || s[i] == 'u' || s[i] == 'o' || s[i] == 'e') {
-            if (!start) {
-                if (check) {
-                    count2++;
-                    ans = (ans * count2) % MOD;
-                }
-            }
-            count++;
-            start = true;
-            check = true;
-            count2 = 0;
-        } else {
-            if (!start) {
-                count2++;
-            }
+  for (int i = 0; i < n; i++)
+  {
+    if (s[i] == 'a' || s[i] == 'i' || s[i] == 'u' || s[i] == 'o' || s[i] == 'e')
+    {
+      if (!st)
+      {
+        if (fl)
+        {
+          vnb++;
+          main = (main * vnb) % MOD;
         }
-        if (count == k) {
-            start = false;
-            count = 0;
-        }
+      }
+      cnt++;
+      st = true;
+      fl = true;
+      vnb = 0;
     }
+    else
+    {
+      if (!st)
+      {
+        vnb++;
+      }
+    }
+    if (cnt == k)
+    {
+      st = false;
+      cnt = 0;
+    }
+  }
 
-    cout << ans << endl;
+  cout << main << endl;
 }
 
-int main() {
+int main()
+{
 
-  int t;cin>>t;
-  while(t--) {
-    int n,m;cin>>n>>m;
-
-
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, m;
+    cin >> n >> m;
   }
-    
-    return 0;
+
+  return 0;
 }
