97aec6f990bf149260c5cdb04e30db4e103258cd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-15 00:57:53 +0530

    1

---
+++ Practice Question/800-2000/E. Character Blocking.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define _test   \
+  int _TEST;    \
+  cin >> _TEST; \
+  while (_TEST--)
+#define ff first
+#define ss second
+#define pb push_back
+#define ppb pop_back
+
+// const int mxn = 200005;
+// ll val[] = {1, 10, 100, 1000, 10000};
+// ll dp[mxn][7][2], n;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t, c[5003];
+  cin >> t;
+  while (t--)
+  {
+    
+
+  }
+}
