dbdf80ad7a02c1d291dd75e7499e3bdf733b7f5f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-14 19:25:39 +0530

    1

--- Practice Question/800-2000/G2. Subsequence Addition (Hard Version).cpp
+++ Practice Question/800-2000/G2. Subsequence Addition (Hard Version).cpp
@@ -15,71 +15,37 @@ const int mxn = 200005;
 ll val[] = {1, 10, 100, 1000, 10000};
 // ll dp[mxn][7][2], n;
 
-void solve()
-{
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; ++i)
-    cin >> a[i];
-
-  sort(a.begin(), a.end());
-  if (a[0] != 1)
-  {
-    cout << "NO\n";
-    return;
-  }
-
-  vector<int> dp(5005, 0);
-
-  dp[1] = 1;
-
-  for (int i = 1; i < n; ++i)
-  {
-    if (!dp[a[i]])
-    {
-      cout << "NO\n";
-      return;
-    }
-    for (int j = 5000; j >= a[i]; --j)
-    {
-      dp[j] |= dp[j - a[i]];
-    }
-  }
-
-  // for (int j = 0; j <5000; j++)
-  //   {
-  //     cout<<dp[j] <<" ";
-  //   }
-
-  //   cout<<endl;
-
-  cout << "YES\n";
-}
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  _test
+  int t, c[5003];
+  cin >> t;
+  while (t--)
   {
-    // ll n;
-    // cin >> n;
-    // ll arr[n];
-
-    // for (ll i = 0; i < n; i++) {
-    //   cin >> arr[i];
-    // }
-
-    // sort(arr,arr+n);
-
-    // if(arr[0]!=-1){
-    //   cout<<"NO"<<endl;
-    //   continue;
-    // }
-
-    solve();
+    int n, f = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+      cin >> c[i];
+    sort(c + 1, c + 1 + n);
+    long long sum = 1;
+    if (c[1] != 1)
+    {
+      f = 1;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+      if (c[i] > sum)
+      {
+        f = 1;
+      }
+      sum += c[i];
+    }
+    if (f == 1)
+      cout << "NO" << endl;
+    else
+      cout << "YES" << endl;
   }
 }
