c7dac314e20ea16983ad00bfb577bfcecf1e235b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-22 02:50:34 +0530

    1

--- Practice Question/800-2000/C. Ski Resort.cpp
+++ Practice Question/800-2000/C. Ski Resort.cpp
@@ -1,31 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
-
-
-
-int main() {
+int main()
+{
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int t; cin >> t;
-  while (t--) {
+  int t;
+  cin >> t;
+  while (t--)
+  {
 
-    ll n,k,q;cin>>n>>k>>q;
+    ll n, k, q;
+    cin >> n >> k >> q;
     vector<ll> v(n);
-    for (int i = 0; i < n; i++) cin>>v[i];
-     
+    for (int i = 0; i < n; i++)
+      cin >> v[i];
 
-
-    
-
-
+      
   }
 }
-
-
-
-
-
