360be96bd6da0bf484269d021d98425ca4b7773d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-22 02:17:26 +0530

    1

--- Practice Question/800-2000/D. Wooden Toy Festival.cpp.cpp
+++ Practice Question/800-2000/D. Wooden Toy Festival.cpp.cpp
@@ -15,6 +15,25 @@ using namespace std;
 // ll val[] = {1, 10, 100, 1000, 10000};
 // ll dp[mxn][7][2], n;
 
+bool solve(int mid,vector<ll> &v){
+  vector<int> res;
+
+  int val=v[0]-mid;
+  res.push_back(val);
+  for (int i = 1; i <v.size(); i++)
+  {
+    /* code */
+    if(abs(val-v[i])>mid){
+      val=v[i]-mid;
+      res.push_back(val);
+    }
+  }
+  
+  if(res.size()<=3) return true;
+  else return false;
+
+}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -23,16 +42,28 @@ int main()
 
   int t, c[5003];
   cin >> t;
-  while (t--)
-  {
+  while (t--) {
     ll n;cin>>n;
+    vector<ll> v(n);
 
-     vector<ll> v(n);
-
-     for (int i = 0; i < n; i++)
-      
-      cin>>v[i];
+    for (int i = 0; i < n; i++) cin>>v[i];
      
+
+
+    sort(v.rbegin(),v.rend());
+    int lo=0;
+    int hi=1e9;
+    int ans=1e9;
+
+    while(hi>=lo){
+      int mid=(lo+hi)/2;
+      if(solve(mid,v)){
+        ans=mid;
+        hi=mid-1;
+      }else {
+        lo=mid+1;
+      }
+    }
      
     
 
