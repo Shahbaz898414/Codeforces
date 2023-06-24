aa90e3c127d5021fde3abeb49cbd72d0a3912e46
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-24 14:49:01 +0530

    1

---
+++ Practice Question/800-2000/B. Binary Cafe.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+
+    int n;
+    cin >> n;
+     int m;cin>>m;
+    // vector<int> v(n + 1);
+    // vector<int> pre(n + 1);
+    // for (int i = 1; i < n + 1; i++) 
+    //   cin >> v[i];
+
+    m=min(m,30);
+
+
+    cout<<min(n+1,(1<<m))<<endl;
+    
+   
+  }
+}
