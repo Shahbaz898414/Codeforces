23176c2923dc11a0c6977848fe004a8e0eaa5986
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-24 15:45:09 +0530

    1

---
+++ Practice Question/800-2000/B. Astrophysicists.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+
+    int n, c1 = 0, c2 = 0, ctr = 0,m,k;
+    cin >> n>>m>>k;
+    
+
+    
+  }
+
+  return 0;
+}
+
