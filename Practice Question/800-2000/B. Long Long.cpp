051fd7337feb52f6bc6c755d2e92954f4dd1ff4a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-30 03:17:48 +0530

    1

--- Practice Question/800-2000/B. Long Long.cpp
+++ Practice Question/800-2000/B. Long Long.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define ll long long int
 
-
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -11,36 +10,39 @@ int main()
 
   int t;
   cin >> t;
-  while (t--) {
-     int n;
-        cin >> n;
-        
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        
-        int sum = 0;
-        int cnt = 0;
-        bool open = false;
-        
-        for (int i = 0; i < n; i++) {
-            sum += abs(a[i]);
-            if (a[i] < 0 && !open) {
-                open = true;
-                cnt++;
-            }
-            if (a[i] > 0) {
-                open = false;
-            }
-        }
-        
-        cout << sum << " " << cnt << endl;
-in>>arr[i];
-    }
-    
-    
-  }
+  while (t--)
+  {
+    int n;
+    cin >> n;
 
-  return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+    }
+
+    int sum = 0;
+    int cnt = 0;
+    bool open = false;
+
+    for (int i = 0; i < n; i++)
+    {
+      sum += abs(a[i]);
+      if (a[i] < 0 && !open)
+      {
+        open = true;
+        cnt++;
+      }
+      if (a[i] > 0)
+      {
+        open = false;
+      }
+    }
+
+    cout << sum << " " << cnt << endl;
+    // in >> arr[i];
+  
+}
+
+return 0;
 }
