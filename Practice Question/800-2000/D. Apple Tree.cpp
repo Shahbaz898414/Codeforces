8bcdb401d6ecaf4e6db64a4f8d3cb0db7b23775b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-30 02:12:15 +0530

    1

--- Practice Question/800-2000/D. Apple Tree.cpp
+++ Practice Question/800-2000/D. Apple Tree.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 #define ll long long
 
+void dfs(ll s, ll p, vector<ll> adj[], ll leaf[])
+{
+  bool any = false;
 
-void dfs(ll s,ll p,vector<ll>adj[],ll leaf[]){
-    bool any=false;
-    
-    for(auto e:adj[s]){
-        if(e==p)
-        continue;
-        any=true;
-        dfs(e,s,adj,leaf);
-        leaf[s]+=leaf[e];
-    }
-    
-    if(!any)
+  for (auto e : adj[s])
+  {
+    if (e == p)
+      continue;
+    any = true;
+    dfs(e, s, adj, leaf);
+    leaf[s] += leaf[e];
+  }
+
+  if (!any)
     leaf[s]++;
 }
 
@@ -24,30 +25,33 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int t; cin >> t;
-  while (t--) {
-    ll n;cin>>n;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    ll n;
+    cin >> n;
 
-    // vector<pair<ll,ll>> v(n);
+    vector<pair<ll,ll>> arr(n);
 
-    vector<ll>  adj[n+1];
+    vector<ll> adj[n + 1];
 
-    for (int i = 0; i < n; i++) {
-      ll x,y;cin>>x>>y;
+    for (int i = 0; i < n; i++)
+    {
+      ll x, y;
+      cin >> x >> y;
       // v.push_back({x,y})
-      adj[y].push_back(x);      
-      adj[x].push_back(y);      
+      adj[y].push_back(x);
+      adj[x].push_back(y);
     }
 
-    ll q;cin>>q;
+    ll q;
+    cin >> q;
 
-    ll leaf[n+1]={0};
+    ll leaf[n + 1] = {0};
 
-    dfs(1,-1,adj,leaf);
-
-    
+    dfs(1, -1, adj, leaf);
   }
-
 
   return 0;
 }
