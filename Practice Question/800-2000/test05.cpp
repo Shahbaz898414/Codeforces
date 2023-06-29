abf9955ac45f3456b62d1990ebc924ff3d831c39
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-29 20:43:34 +0530

    1

--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -1,42 +1,42 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-// const ll n = 1e5 + 2;
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long int ll;
+// // const ll n = 1e5 + 2;
 
-int main()
-{
+// int main()
+// {
 
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, k, x;
-        cin >> n >> k >> x;
-        if ((n % 2 and k % 2 == 0 and x == 1) || (n % 2 and k % 2 and x == 1) || (n % 2 == 0 and k % 2 and x == 1))
-        {
-            cout << "NO" << endl;
-        }
-        else if (n % 2 == 0 and k % 2 == 0 and x % 2)
-        {
-            cout << "YES" << endl;
-            cout << n / 2 << endl;
-            for (int i = 2; i <= n; i + 2)
-            {
-                cout << i << " ";
-            }
-            cout << endl;
-        }
-        else
-        {
-            cout << "YES" << endl;
+//     ll t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         ll n, k, x;
+//         cin >> n >> k >> x;
+//         if ((n % 2 and k % 2 == 0 and x == 1) || (n % 2 and k % 2 and x == 1) || (n % 2 == 0 and k % 2 and x == 1))
+//         {
+//             cout << "NO" << endl;
+//         }
+//         else if (n % 2 == 0 and k % 2 == 0 and x % 2)
+//         {
+//             cout << "YES" << endl;
+//             cout << n / 2 << endl;
+//             for (int i = 2; i <= n; i + 2)
+//             {
+//                 cout << i << " ";
+//             }
+//             cout << endl;
+//         }
+//         else
+//         {
+//             cout << "YES" << endl;
 
-            cout << n << endl;
-            for (int i = 1; i <= n; i++)
-            {
-                cout << i << " ";
-            }
+//             cout << n << endl;
+//             for (int i = 1; i <= n; i++)
+//             {
+//                 cout << i << " ";
+//             }
 
-            cout << endl;
-        }
-    }
-}
+//             cout << endl;
+//         }
+//     }
+// }
