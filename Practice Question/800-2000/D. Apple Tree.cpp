5ebadb83c917927b81f84968ac57e1b0daf5a31e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-30 02:11:57 +0530

    1

--- Practice Question/800-2000/D. Apple Tree.cpp
+++ Practice Question/800-2000/D. Apple Tree.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 #define ll long long
 
+
+void dfs(ll s,ll p,vector<ll>adj[],ll leaf[]){
+    bool any=false;
+    
+    for(auto e:adj[s]){
+        if(e==p)
+        continue;
+        any=true;
+        dfs(e,s,adj,leaf);
+        leaf[s]+=leaf[e];
+    }
+    
+    if(!any)
+    leaf[s]++;
+}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -12,14 +28,23 @@ int main()
   while (t--) {
     ll n;cin>>n;
 
-    vector<pair<ll,ll>> v(n);
+    // vector<pair<ll,ll>> v(n);
+
+    vector<ll>  adj[n+1];
 
     for (int i = 0; i < n; i++) {
       ll x,y;cin>>x>>y;
-      
+      // v.push_back({x,y})
+      adj[y].push_back(x);      
+      adj[x].push_back(y);      
     }
 
     ll q;cin>>q;
+
+    ll leaf[n+1]={0};
+
+    dfs(1,-1,adj,leaf);
+
     
   }
 
