41da47532cc3961182c29212d6134e60b041301e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-23 20:30:35 +0530

    1

--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 typedef long long int ll;
 
-
 int main()
 {
 
-    ll t;
-    cin >> t;
-    while (t--) {
-      ll n, k, x=0,cnt;
-      cin >> n;
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+    ll n, k, x = 0, cnt;
+    cin >> n;
 
-     
-    
-
-        
-
-
-      
-
-
-        
+    vector<ll> arr(n);
+    for (ll i = 0; i < n; i++)
+    {
+      cin >> arr[i];
     }
+    ll mi = INT_MAX;
+
+    for (ll i = 1; i < n; ++i)
+    {
+      mi = min(mi, abs(arr[i] - arr[i - 1]));
+    }
+    cout << (mi / 2 + 1) << endl;
+  }
 }
