fb2356f1d041fbcb6a5f023a951310c24e00fdd5
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-23 20:03:37 +0530

    1

--- Practice Question/800-2000/C. Tenzing and Balls.cpp
+++ Practice Question/800-2000/C. Tenzing and Balls.cpp
@@ -12,37 +12,15 @@ int main()
   cin >> t;
   while (t--)
   {
-    ll n;
-    cin >> n;
-    ll a[n];
+    ll n; cin >> n;
+    vector<ll> a(n);
     for (ll i = 0; i < n; i++)
       cin >> a[i];
+    
     ll ans = 0;
     ll op = 0;
 
-    sort(a, a + n);
-
-    for (ll i = 0; i < n/2; i++)
-    {
-      ans+=a[n-i-1]-a[i];
-    }
-
-    cout<<ans<<endl;
-
-   
-
-    /*
-
-    7
-    0
-    11
-    23
-    0
-    5
-
-    1 13 9 3 7 2
-
-    */
+    
   }
 
   return 0;
--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -8,35 +8,19 @@ int main()
 
     ll t;
     cin >> t;
-    while (t--)
-    {
-        ll n, k, x;
-        cin >> n >> k >> x;
-        if ((n % 2 and k % 2 == 0 and x == 1) || (n % 2 and k % 2 and x == 1) || (n % 2 == 0 and k % 2 and x == 1))
-        {
-            cout << "NO" << endl;
-        }
-        else if (n % 2 == 0 and k % 2 == 0 and x % 2)
-        {
-            cout << "YES" << endl;
-            cout << n / 2 << endl;
-            for (int i = 2; i <= n; i + 2)
-            {
-                cout << i << " ";
-            }
-            cout << endl;
-        }
-        else
-        {
-            cout << "YES" << endl;
+    while (t--) {
+      ll n, k, x=0,cnt;
+      cin >> n;
 
-            cout << n << endl;
-            for (int i = 1; i <= n; i++)
-            {
-                cout << i << " ";
-            }
+     
+    
 
-            cout << endl;
-        }
+        
+
+
+      
+
+
+        
     }
 }
