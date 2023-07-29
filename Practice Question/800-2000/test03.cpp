5f5a8e18ca8111d67735a1baf41b9167ba83c42a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-29 20:55:09 +0530

    1

--- Practice Question/800-2000/test03.cpp
+++ Practice Question/800-2000/test03.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 
 
 
@@ -8,9 +10,22 @@ int main()
 {
   int t;
   cin >> t;
-  while (t--)
+  while (t-->0)
   {
-   
+
+    // string s;cin>>s;
+    ll n;cin>>n;
+
+    // vector<ll>  arr(n);
+
+    ll cnt=1;
+
+   while(n%cnt==0){
+    cnt++;
+   }
+
+    cout<<cnt-1<<endl;
+    
        
     
   }
