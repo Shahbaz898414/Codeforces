cb7bcb3e962acaa1e8effa803b78f715fd377945
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-02 04:42:22 +0530

    1

--- Practice Question/800-2000/test1.cpp
+++ Practice Question/800-2000/test1.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 using namespace std;
 
 
-
-vector<int>  prefix(string s){
+vector<int>  prefix1(string s){
     int n=s.size();
     vector<int> pi(n,0);
     for (int i = 1; i <n; i++)
@@ -24,12 +22,44 @@ vector<int>  prefix(string s){
     }
 
     return pi;
-       
 }
 
 
 
 int main() {
-   
+   string pat="sh";
+
+   vector<int> prefix=prefix1(pat);
+   string txt="shnknfncshcmclshnuxshnnmcsh";
+
+    int pos=-1;
+    int i(0);
+    int j(0);
+    vector<int>  v;
+    while(i<txt.size()){
+        if(txt[i]==pat[j]){
+            j++;
+            i++;
+        }else{
+            if(j!=0){
+                j=prefix[j-1];
+            }else i++;
+        }
+        if(j==pat.size()){
+             pos=i-pat.size();
+            v.push_back(pos+1);
+           
+            // break;
+        }
+    }
+
+   for(auto it:v){
+    cout<<it<<" ";
+   }
+
+//    cout<<endl;
+
+
+
     return 0;
 }
