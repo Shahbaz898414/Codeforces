9578cb97c14ee422f7999aeaf1cb059ed174288f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-25 18:11:24 +0530

    1

--- Practice Question/800-2000/E. Power of Points.cpp
+++ Practice Question/800-2000/E. Power of Points.cpp
@@ -1,4 +1,4 @@
-/*
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -171,6 +171,12 @@ signed main()
 
     sort(arr.begin() + 1, arr.end());
 
+    for(auto it:arr){
+      cout<<it.first<<" "<<it.second<<endl;
+    }
+
+
+    cout<<endl;
     vector<ll> psum(n + 1, 0);
 
     for (ll i = 1; i <= n; i++)
@@ -179,12 +185,24 @@ signed main()
       psum[i] = psum[i - 1] + arr[i].ff;
     }
 
+
+    for (ll i = 1; i <=n; i++)
+    {
+      /* code */
+      cout<<psum[i]<<" ";
+    }
+
+    cout<<endl;
+
+    cout<<endl;
+    
+
     vector<ll> ans(n + 1, n);
 
     for (ll i = 1; i <= n; i++)
     {
       
-      ans[arr[i].ss] = (psum[n] - psum[i] - (n - i) * arr[i].first);
+      ans[arr[i].ss] += (psum[n] - psum[i] - (n - i) * arr[i].first);
 
       ans[arr[i].ss] += ((i - 1) * arr[i].first - psum[i - 1]);
     }
@@ -203,7 +221,6 @@ signed main()
 
 
 
-*/
 
 
 
@@ -220,137 +237,6 @@ signed main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Header Files
-#include<iostream>
-#include<iomanip>
-#include<algorithm>
-#include<vector>
-#include<utility>
-#include<set>
-#include<unordered_set>
-#include<list>
-#include<iterator>
-#include<deque>
-#include<queue>
-#include<stack>
-#include<set>
-#include<bitset>
-#include<random>
-#include<map>
-#include<unordered_map>
-#include<stdio.h>
-#include<complex>
-#include<math.h>
-#include<cstring>
-#include<chrono>
-#include<string>
-// #include "ext/pb_ds/assoc_container.hpp"
-// #include "ext/pb_ds/tree_policy.hpp"
-// Header Files End
-
-using namespace std;
-using namespace __gnu_pbds;
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
-template<typename T> 
-using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
-
-template<class key, class value, class cmp = std::less<key>>
-using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
-// find_by_order(k)  returns iterator to kth element starting from 0;
-// order_of_key(k) returns count of elements strictly smaller than k;
-
-#define DIVYA ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define ll long long
-#define umap unordered_map
-#define uset unordered_set
-#define lb lower_bound
-#define ub upper_bound
-#define fo(i,a,b) for(i=a;i<=b;i++)
-#define all(v) (v).begin(),(v).end()
-#define all1(v) (v).begin()+1,(v).end()
-#define allr(v) (v).rbegin(),(v).rend()
-#define allr1(v) (v).rbegin()+1,(v).rend()
-#define sort0(v) sort(all(v))
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<ll, ll> pll;
-#define sz(x) (ll)x.size()
-#define pb push_back
-#define ppb pop_back
-#define mkp make_pair
-#define inf 1000000000000000005
-const ll mod = 1e9 + 7;
-
-ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
-
-ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
-
-ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
-
-ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
-
-ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
-
-ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
-
-
-int main() {
-    DIVYA;
-    ll t, n, i, j, ans, temp, sum;
-    string sans;
-    t = 1;
-    cin >> t;
-    while (t--)
-    {
-        sans = "NO";
-        ans = temp = sum = 0;
-        cin >> n;
-        vector<pll>a(n+1);
-        fo(i,1,n)
-        {
-            cin>>a[i].first;
-            a[i].second = i;
-        }
-        sort(all1(a));
-        vll psum(n+1,0);
-        fo(i,1,n)
-        {
-            psum[i] = psum[i-1] + a[i].first;
-        }
-        vll ansvec(n+1,n);
-
-        fo(i,1,n)
-        {
-            //a[j] - a[i]
-            ansvec[a[i].second]+=(psum[n] - psum[i] - (n - i)*a[i].first);
-
-            //a[i] - a[j];
-            ansvec[a[i].second]+=((i-1)*a[i].first - psum[i-1]);
-        }
-
-        fo(i,1,n)
-        {
-            cout<<ansvec[i]<<" ";
-        }
-        cout<<"\n";
-
-    }
-    return 0;
-}
 
 
 
