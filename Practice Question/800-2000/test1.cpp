a25c960871e4c377f104a26daaef98d7b0819ed7
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-26 19:26:09 +0530

    1

--- Practice Question/800-2000/test1.cpp
+++ Practice Question/800-2000/test1.cpp
@@ -4,26 +4,59 @@
 using namespace std;
 #define ll long long
 
+ll mod =1e9+7;
+
 
 
 
 int main() {
+
+    ll dp[1005][1005];
+
+    dp[1][1]=1;
+
+    for (ll i =2; i < 1005; i++)
+    {
+        /* code */
+        for (ll j = 1; j <=i; j++)
+        {
+            /* code */
+
+            dp[i][j]=(dp[i-1][j-1]+dp[i-1][j]*j)%mod;
+        }
+        
+    }
+    
+
+
+
 
   ll t;cin>>t;
 
   while(t--) {
     ll n;cin>>n;
 
-    vector<ll>  arr(n);
+    ll ans=0;
 
-    for (ll i = 0; i < n; i++)   
-        cin>>arr[i];
+
+    for (ll i = 0; i <=n; i++)
+    {
+        /* code */
+        ans=(ans+dp[n][i])%mod;
+    }
+
+    cout<<ans<<endl;
+    
+
+    // vector<ll>  arr(n);
+
+    // for (ll i = 0; i < n; i++)   
+    //     cin>>arr[i];
     
     
 
 
 
-    
   }
 
 
