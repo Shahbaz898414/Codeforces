0fd4faea418924b9bfad2c913b1794760ea6001f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-02 01:48:23 +0530

    1

--- Practice Question/800-2000/E. Cardboard for Pictures.cpp
+++ Practice Question/800-2000/E. Cardboard for Pictures.cpp
@@ -146,41 +146,40 @@ long long erfd(long long a, long long b)
     return ans % m * ans % m;
 }
 
+void solve()
+{
 
-
-
-void solve() {
-
-
-    int n, c; cin >> n >> c;
-    vector<int> a(n);
-    for(int i = 0; i < n; ++i) cin >> a[i];
-    int l = 1, r = 1e9;
-    while(l <= r) {
-        int mid = l + (r - l) / 2;
-        int sumall = 0;
-        for(int i = 0; i < n; ++i) {
-            sumall += (a[i] + 2 * mid) * (a[i] + 2 * mid);
-            if(sumall > c) break;
-        }
-        if(sumall == c) {
-            cout << mid << "\n";
-            return;
-        }
-        if(sumall > c) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
+  int n, c;
+  cin >> n >> c;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i)
+    cin >> a[i];
+  int l = 1, r = 1e9;
+  while (l <= r)
+  {
+    int mid = l + (r - l) / 2;
+    int sumall = 0;
+    for (int i = 0; i < n; ++i)
+    {
+      sumall += (a[i] + 2 * mid) * (a[i] + 2 * mid);
+      if (sumall > c)
+        break;
     }
-
-
-}  
-
-
-
-
-
+    if (sumall == c)
+    {
+      cout << mid << "\n";
+      return;
+    }
+    if (sumall > c)
+    {
+      r = mid - 1;
+    }
+    else
+    {
+      l = mid + 1;
+    }
+  }
+}
 
 signed main()
 {
@@ -195,5 +194,3 @@ signed main()
   }
   return 0;
 }
-
-
