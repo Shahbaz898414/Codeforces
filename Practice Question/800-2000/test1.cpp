37178ce52c24ff14d3508df9f21901d3b8a205ac
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-14 18:23:06 +0530

    1

--- Practice Question/800-2000/test1.cpp
+++ Practice Question/800-2000/test1.cpp
@@ -37,3 +37,66 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+
+
+ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *left_ptr = head, *right_ptr = head;
+        for (int i = 0; i < k-1; i++) {
+            right_ptr = right_ptr->next;
+        }
+        
+        ListNode *end_ptr = right_ptr;
+        while (right_ptr->next) {
+            left_ptr = left_ptr->next;
+            right_ptr = right_ptr->next;
+        }
+        
+        swap(end_ptr->val, left_ptr->val);
+        
+        return head;
+}
+
+
+
+
+*/
+
+
+/*
+
+
+
+ ListNode* swapNodes(ListNode* head, int k) {
+    if(head->next==NULL){
+        return head;
+    }
+    ListNode*temp=head;
+    int s=0;
+    while(temp!=NULL){
+        temp=temp->next;
+        s++;
+    }
+    ListNode*n1=head;
+      int t=1;
+      while(t!=k){
+         n1=n1->next;
+         t++;
+      }
+      ListNode*n2=head;
+      t=1;
+      while(t!=s+1-k){
+          n2=n2->next;
+          t++;
+      }
+      swap(n1->val,n2->val);
+      return head;
+      
+      
+}
+
+
+*/
