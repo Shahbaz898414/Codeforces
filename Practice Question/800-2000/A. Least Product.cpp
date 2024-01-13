5e3d9285fc7aba53057eba440f124909095ce4aa
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-13 16:44:51 +0530

    1

--- Practice Question/800-2000/A. Least Product.cpp
+++ Practice Question/800-2000/A. Least Product.cpp
@@ -16,6 +16,8 @@ int main()
     cin >> n;
     int a[n];
     int s = 0, f0 = 0;
+
+    
     for (int i = 0; i < n; ++i)
     {
       cin >> a[i];
@@ -41,3 +43,6 @@ int main()
 
   return 0;
 }
+
+
+
