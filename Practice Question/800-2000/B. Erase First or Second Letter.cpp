d3db1dfee84bc7aa4802acee4abf727584bafa28
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-16 19:14:51 +0530

    1

--- Practice Question/800-2000/B. Erase First or Second Letter.cpp
+++ Practice Question/800-2000/B. Erase First or Second Letter.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main()
+{
 
+  int t;
+  cin >> t;
 
-int main() {
-   
-    int n;cin>>n;
+  while (t--)
+  {
+    int n;
+    cin >> n;
 
-    
-    
-  
-   
+    string s;
+    cin >> s;
 
-    
+    set<char> se;
 
-    return 0;
+    int ans=0;
+
+    for (int i = 0; i < n; i++)
+    {
+      se.insert(s[i]);
+
+      ans+=se.size();
+    }
+
+    // for (auto it : se)
+    // {
+    //   cout << it << " ";
+    // }
+
+    // cout << endl;
+
+    cout << ans<< endl;
+  }
+
+  return 0;
 }
 
+
+
+/*
+
+
+ababa 1
+a baba 2
+b aaba 2
+aba bba 2
+ba aa 2
+
+
+
+
+
+
+*/
