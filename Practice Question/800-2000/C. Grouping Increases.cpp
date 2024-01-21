22afb028d79597a4b3e5f1c6aa98c876ce0d9a16
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-21 20:59:42 +0530

    1

---
+++ Practice Question/800-2000/C. Grouping Increases.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+// #ifndef ONLINE_JUDGE
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+// #endif
+
+  int tt;
+  cin >> tt;
+
+  while (tt--)
+  {
+    int n;cin>>n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+      
+      cin>>arr[i];
+    }
+    
+    
+  }
+}
--- Practice Question/800-2000/D. Array Repetition.cpp
+++ Practice Question/800-2000/D. Array Repetition.cpp
@@ -1,53 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 
 signed main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+// #ifndef ONLINE_JUDGE
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+// #endif
 
-    int tt; cin >> tt;
+  int tt;
+  cin >> tt;
 
-    while(tt--) {
-        int n, q, sz = 0; cin >> n >> q;
+  while (tt--)
+  {
+    int n, q, sz = 0;
+    cin >> n >> q;
 
-        vector<pair<int,int>>a(n);
-        for(auto &i : a) cin >> i.first >> i.second;
+    vector<pair<int, int>> a(n);
+    for (auto &i : a)
+      cin >> i.first >> i.second;
 
-        vector<int>queries(q), sizes;
-        for(auto &i : queries) cin >> i;
+    vector<int> queries(q), sizes;
+    for (auto &i : queries)
+      cin >> i;
 
-        for(auto it : a) {
+    for (auto it : a)
+    {
 
-            sizes.push_back(sz + 1);
+      sizes.push_back(sz + 1);
 
-            if(it.first == 1) {
-                sz++;
-            }
-            else {
-                if(sz + 1 <= 2e18 / it.second) {
-                    sz += sz * it.second;
-                }
-                else break;
-            }
+      if (it.first == 1)
+        sz++;
+      
+      else
+      {
+        if (sz + 1 <= 2e18 / it.second)
+        {
+          sz += sz * it.second;
         }
-
-        function<int(int)>processQuery = [&](int number) {
-            auto idx = upper_bound(sizes.begin(), sizes.end(), number) - sizes.begin() - 1;
-            if(a[idx].first == 1) return a[idx].second;
-            else return processQuery((number % (sizes[idx] - 1) == 0 ? sizes[idx] - 1 : number % (sizes[idx] - 1)));
-        };
-
-
-        for(auto &i : queries) cout << processQuery(i) << " ";  cout << "\n";
-
-        
+        else
+          break;
+      }
     }
+
+
+
+    function<int(int)> processQuery = [&](int number)
+    {
+      auto idx = upper_bound(sizes.begin(), sizes.end(), number) - sizes.begin() - 1;
+      if (a[idx].first == 1)
+        return a[idx].second;
+      else
+        return processQuery((number % (sizes[idx] - 1) == 0 ? sizes[idx] - 1 : number % (sizes[idx] - 1)));
+    };
+
+    for (auto &i : queries)
+      cout << processQuery(i) << " ";
+    cout << "\n";
+  }
 }
