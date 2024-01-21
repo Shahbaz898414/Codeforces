eec2832313f9d390b2e3bfd70f5335943cddee75
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-21 21:34:03 +0530

    1

--- Practice Question/800-2000/C. Grouping Increases.cpp
+++ Practice Question/800-2000/C. Grouping Increases.cpp
@@ -18,15 +18,24 @@ signed main()
 
   while (tt--)
   {
-    int n;cin>>n;
+    int n;
+		cin >> n;
+ 
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) cin >> a[i];
+ 
+		int penalty = 0;
+		int lo = n + 1, hi = n + 1;
+ 
+		for (int i = 0; i < n; i++) {
+			if (lo > hi) swap(lo, hi);
+			if (a[i] <= lo) lo = a[i];
+			else if (a[i] > hi) penalty++, lo = a[i];
+			else hi = a[i];
+		}
+ 
+		cout << penalty << endl;
 
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-      
-      cin>>arr[i];
-    }
-    
     
   }
 }
