901c43c2f0f22bbab05cc927548c9a96f6943b97
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-27 15:22:42 +0530

    1

---
+++ Practice Question/800-2000/C. Quests.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+long long gcd(long long a, long long b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+long long lcm(int a, int b)
+{
+  return (a / gcd(a, b)) * b;
+}
+
+
+void solve() {
+
+  ll n,k;cin>>n>>k;
+
+  vector<ll> arr(n),brr(n);
+
+  for (ll i = 0; i < n; i++) {
+    cin>>arr[i];
+  }
+
+  for (ll i = 0; i < n; i++) {
+    cin>>brr[i];
+  }
+  
+  
+
+
+  
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int tt;
+  cin >> tt;
+
+  while (tt--)
+  {
+
+    solve();
+  }
+}
---
+++ Practice Question/800-2000/D. Split Plus K.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+long long gcd(long long a, long long b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+long long lcm(int a, int b)
+{
+  return (a / gcd(a, b)) * b;
+}
+
+void solve()
+{
+
+  ll n, k;
+  cin >> n >> k;
+
+  vector<ll> arr(n);
+
+  for (ll i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+
+  
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int tt;
+  cin >> tt;
+
+  while (tt--)
+  {
+
+    solve();
+  }
+}
