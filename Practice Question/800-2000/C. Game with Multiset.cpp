88848c6062378d860ba99cf8a9000307ceb5b952
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-30 18:17:19 +0530

    1

---
+++ Practice Question/800-2000/C. Game with Multiset.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+long long gcd(long long a, long long b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+long long lcm(int a, int b)
+{
+  return (a / gcd(a, b)) * b;
+}
+
+void solve()
+{
+
+  int n;cin>>n;
+
+  
+
+
+
+
+}
+
+
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int tt;
+  cin >> tt;
+
+  while (tt--)
+  {
+
+    solve();
+  }
+}
--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -1,138 +1,98 @@
-#include <iostream>
-
-#include <bits/stdc++.h>
-#include <string> // Include the necessary header for string
+#include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long
 
-int main()
-{
-    int t;
-    cin >> t;
+int numberOfPoints(vector<vector<int>>& nums) {
 
-    while (t--)
-    {
-        ll n;
-        cin >> n;
+    int sz = nums.size();
+    vector<int> pref(200);
 
-        vector<ll> fhgt(n);
-
-        for (int i = 0; i < n; i++) {
-            cin >> fhgt[i];
+        for(auto &num : nums){
+            pref[num[0]] += 1;
+            pref[num[1]+1] -= 1;
         }
 
-        if (n <= 2)
-            cout << -1 << endl;
-        else if (n == 3)
-        {
-            ll rt, gf;
-              // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
 
 
-            // fhgt[jg] = 3+(fhgt[jn]-1);
+        for(int indx = 0; indx < pref.size(); indx++){
+           if(indx != 0) 
+            pref[indx] += pref[indx-1];
 
-            for (int i = 0; i < n; i++)
-            {
-
-                  // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
-
-
-            // fhgt[jg] = 3+(fhgt[jn]-1);
-
-                if (fhgt[i] == 1) rt = i;
-                if (fhgt[i] == 3) gf = i;
-
-                  // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
-
-
-            // fhgt[jg] = 3+(fhgt[jn]-1);
-
-            }
-
-
-            fhgt[rt] = 3;
-
-
-            fhgt[gf] = 1+(fhgt[rt]-3);
-
-
-            for (int i = 0; i < n; i++) cout << fhgt[i] << " ";
-
-
-            cout << endl;
+            // cout<<pref[indx]<<" ";
         }
-        else
-        {
 
-            ll jg, jn;
-            for (int i = 0; i < n; i++) {
-                  // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
+        // cout<<endl;
 
 
-            // fhgt[jg] = 3+(fhgt[jn]-1);
+        int zeros = count(pref.begin(), pref.end(), 0);
 
-                if (fhgt[i] == 1) jg = i;
-                if (fhgt[i] == 3) jn = i;
-
-                  // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
+        // cout<<zeros<<endl;
 
 
-            // fhgt[jg] = 3+(fhgt[jn]-1);
-
-            }
-
-                fhgt[jn] = 1;
+        return pref.size() - zeros;
 
 
-            fhgt[jg] = 3+(fhgt[jn]-1);
-
-
-            
-
-
-            for (int i = 0; i < n; i++) cout << fhgt[i] << " ";
-
-  // for (int i = 0; i < n; i++) {
-            //     if (fhgt[i] == 1) jg = i;
-            //     if (fhgt[i] == 3) jn = i;
-            // }
-
-            //     fhgt[jn] = 1;
-
-
-            // fhgt[jg] = 3+(fhgt[jn]-1);
-
-            cout << endl;
-        }
-    }
-
-    return 0;
 }
 
 
+
+
+int main()
+{
+
+    int n;cin>>n;
+
+    vector<vector<int>> arr(n,vector<int>(2));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+
+      cout<<numberOfPoints(arr);  
+    
+
+    // cout << numberOfArithmeticSlices(arr);
+}
+
+
+
+
+/*
+
+Subject: Re: Job Inquiry - Response to your Questions
+
+Hi Jeevitha Mohan,
+
+I hope this email finds you well. Thank you for considering my profile. Please find my responses to your queries below:
+
+Location:
+I am currently based in Mumbai. However, I am open to relocating to Bengaluru if the opportunity aligns with my career goals.
+
+Relocation:
+Yes, I am open to relocating to Bengaluru. I am willing to take care of the relocation process on my own.
+
+Stipend:
+I am comfortable with the stipend range of 15-20k per month for the initial 6 months.
+
+Joining Time:
+I am an immediate joiner and can commence my employment as soon as required. My earliest possible start date is 7.
+
+JD Awareness:
+NO
+
+Github Link:
+Here is the link to my GitHub profile: https://github.com/Shahbaz898414
+
+I am enthusiastic about the opportunity to contribute to [Company Name], and I am looking forward to the possibility of discussing my application further.
+
+Thank you once again for considering my application.
+
+Best regards,
+shahbaz Khan
+7710968756
+
+
+*/
