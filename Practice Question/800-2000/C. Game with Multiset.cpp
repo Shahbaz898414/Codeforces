2cd2706bee49ede64f9fe602415a9650521f6e0b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-31 21:56:10 +0530

    1

--- Practice Question/800-2000/C. Game with Multiset.cpp
+++ Practice Question/800-2000/C. Game with Multiset.cpp
@@ -17,7 +17,27 @@ long long lcm(int a, int b)
 void solve()
 {
 
-  int n;cin>>n;
+    int m;
+    cin >> m;
+    
+    vector<int> bits (31, 0);
+    
+    while (m--) {
+        int t, v;
+        cin >> t >> v;
+        
+        if (t == 1) {
+            bits[v]++;
+        }
+        
+        else {
+            for (int i = 29; i >= 0; i--) {
+                int mn = min (v >> i, bits[i]);
+                v -= mn * (1 << i);
+            }
+            cout << (v == 0 ? "YES" : "NO") << endl;
+        }
+    }
 
   
 
