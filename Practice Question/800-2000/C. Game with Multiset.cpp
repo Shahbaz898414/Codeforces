62fdae66d6a6dd0f4dccc73daf160851ec1112df
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-01-31 22:42:57 +0530

    1

--- Practice Question/800-2000/C. Game with Multiset.cpp
+++ Practice Question/800-2000/C. Game with Multiset.cpp
@@ -52,12 +52,12 @@ signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int tt;
-  cin >> tt;
+  // int tt;
+  // cin >> tt;
 
-  while (tt--)
-  {
+  // while (tt--)
+  // {
 
     solve();
-  }
+  // }
 }
