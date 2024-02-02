185219aa73f749fcaecc2ca81e2bfca2859b5d39
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-03 02:04:50 +0530

    1

--- Practice Question/800-2000/A. Two Permutations.cpp
+++ Practice Question/800-2000/A. Two Permutations.cpp
@@ -72,16 +72,3 @@ signed main()
 
 
 
-
-
-/*
-
-we had given some string we were being some string and
-we were being given N, K and we had to create a string now it is like you have to design a way of confirming whether this string actually whether the string actaully contains all of the given strings or  not.
-
-
-we have been given N, K and a string and we have to check if the string S contains all of the strings that can be generated of length N with the first K english lowercase alphabets whether all the those strings are comming as a subsequence in the string S or not in case if there is any string which it is miising have to print that string so it is kind of like
-
- 
-
-*/
