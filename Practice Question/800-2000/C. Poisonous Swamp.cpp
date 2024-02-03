1e6bd6013a5a71bf492288edc60db5e8b6502a2f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-04 02:30:32 +0530

    1

---
+++ Practice Question/800-2000/C. Poisonous Swamp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+long long gcd(long long a, long long b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+long long lcm(int a, int b)
+{
+  return (a / gcd(a, b)) * b;
+}
+
+void solve()
+{
+
+
+
+
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int tt;
+  cin >> tt;
+
+  while (tt--)
+  {
+
+    solve();
+  }
+}
