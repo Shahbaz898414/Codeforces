7e6a7704b9105dcc6d257e35152c3ee95691a60e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-19 00:06:05 +0530

    1

---
+++ Practice Question/800-2000/F. Chat Screenshots.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define mod 998244353
+
+bool check(int k, vector<vector<int>> &seg)
+{
+  int ll = 0, rr = 0;
+  for (auto &e : seg)
+  {
+    ll = max(ll - k, e[0]);
+    rr = min(rr + k, e[1]);
+    if (ll > rr)
+      return false;
+  }
+
+  return true;
+}
+
+int solve(vector<vector<int>> &seg)
+{
+  int l = -1, r = 1e9;
+  while (r - l > 1)
+  {
+    int mid = (r + l) / 2;
+    if (check(mid, seg))
+    {
+      r = mid;
+    }
+    else
+    {
+      l = mid;
+    }
+  }
+  return r;
+}
+
+int main() {
+
+  int t; cin >> t;
+  while (t--) {
+
+   int n;cin>>n;
+
+   vector<int> arr(n);
+
+   for(int i = 0; i <n; i++) {
+      cin>>arr[i];
+   }
+   
+    
+  }
+}
