3cd964ad6b4a3c16630f060a3a0d79d96037353e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-21 01:14:43 +0530

    1

--- Practice Question/800-2000/E. Anna and the Valentine's Day Gift.cpp
+++ Practice Question/800-2000/E. Anna and the Valentine's Day Gift.cpp
@@ -39,33 +39,26 @@ int solve(vector<vector<int>> &seg)
 int main() {
   int t;cin >> t;
   while (t--) {
-    ll n, m; cin >> n >> m;
-
-    vector<ll> arr(n),zero;
-
-    for (ll i = 0; i < n; i++) {
-      cin >> arr[i];
-    }
-
-    for(auto &i:arr){
-      int ten=0,dig=0;
-      while(i%10==0) ten++,i/=10;
-      while(i)dig++,i/=10;
-
-      m-=dig;
-      zero.push_back(ten);
-    }
-
-
-    sort(zero.begin(),zero.end());
-
-
-    for(int i=1;i<zero.size();i+=2){
-      m-=zero[i];
-    }
-
-
-    cout<<(m<0? "Sasha":"Anna")<<endl;
+    int n, m,sum=0; cin >> n >> m;
+        vector<int>a(n), zeroes;
+        for(auto &i : a) cin >> i;
+ 
+ 
+        for(auto &i : a) {
+            int ten = 0, dig = 0;
+ 
+            while(i % 10 == 0) ten++, i /= 10;
+            while(i) dig++, i /= 10;
+ 
+            sum += dig;
+            zeroes.push_back(ten);
+        }
+ 
+        sort(zeroes.begin(), zeroes.end(), greater<int>());
+ 
+        for(int i = 1; i < zeroes.size(); i += 2) sum+= zeroes[i];
+ 
+        cout << (sum > m ? "Sasha" : "Anna") << "\n";
 
 
   }
@@ -81,14 +74,9 @@ int main() {
 /*
 
 
-The game ends when Sasha can't make a move, i.e. after Anna's move there is exactly one number left in the list.
-If this integer is not less than 10^m (i.e., ≥ 10^m), Sasha wins. Otherwise, Anna wins.
+Anna reverses a single element's digits, while Sasha combines and inserts two elements from the list, reducing its size by one each time. Players take turns removing numbers from a list until only one remains; if that number is ≥ 10^M, Sasha wins; otherwise, Anna wins. Very imp thing final element should have atleast M+1 digit. Anna strategy is pick element with most ending zeroes and reverse it.Sasha strategy follows by selecting the largest ending zero and combines it with another number.
 
-Very imp thing final element should have alteast m+1 digit.
 
-Anna strategy is pick element with most ending zeroes and reverse it.
-
-Sasha turn, she does the same she picks the largest ending zero and concatenates it with some other number
 
 */
 
