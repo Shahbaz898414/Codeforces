6455ca075647a40e7a6e0f8cf4ee3ebac4e2cebf
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-23 20:45:50 +0530

    1

--- Practice Question/800-2000/test03.cpp
+++ Practice Question/800-2000/test03.cpp
@@ -1,149 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
-#define int long long
-// #define mod 998244353
 using namespace std;
 
-const int mod = 998244353;
-
-
-
-signed main()
-{
-
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int T;
-    cin >> T;
-
-    while (T--)
-    {
-        int n, k,cnt=0;
-        cin >> n >> k;
-
-        vector<int> permutation(n);
-
-        if (k > ((n - 1) * (n - 1)) || k < (((n - 1) * n) / 2))
-        {
-            cout << -1 << endl;
+bool isGoodSubarray(const vector<int>& c, int l, int r) {
+    for (int i = l - 1; i < r; ++i) {
+        if (c[i] % 2 != 0) {
+            return false;  // Element is not divisible by 2, so b[i] won't be an integer
         }
-        else if (n == 1)
-        {
-            cout << 1 << endl;
+    }
+    return true;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, q;
+        cin >> n >> q;
+
+        vector<int> c(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> c[i];
         }
-        else
-        {
-            vector<int> per(n, 0);
 
-             for (int i = 0; i < k; ++i)
-            {
-                swap(permutation[i], permutation[i + 1]);
+        while (q--) {
+            int l, r;
+            cin >> l >> r;
+
+            if (isGoodSubarray(c, l, r)) {
+                cout << "YES" << endl;
+            } else {
+                cout << "NO" << endl;
             }
-            per[0] = 1;
-            vector<int> bper(n, 0);
-
-            
-
-            for (int i = 1; i < n; i++)
-            {
-                bper[i] = i;
-                k -= i;
-            }
-
-            
-
-            int left = n - 2;
-
-            while (k)
-            {
-                int rte = n - 1 - bper[left];
-                rte = min(rte, k);
-                k -= rte;
-                bper[left] += rte;
-                left--;
-            }
-
-            int prefSum = 0;
-
-            per[0] = 1;
-
-            for (int i = 0; i < n; ++i)
-            {
-                if (i < k)
-                {
-                    permutation[i] = i + 2;
-                }
-                else if (i == k)
-                {
-                    permutation[i] = 1;
-                }
-                else
-                {
-                    permutation[i] = i + 1;
-                }
-            }
-
-            set<int> as, bs;
-
-            for (int i = 1; i <= n; i++)
-            {
-                as.insert(i);
-                bs.insert(1 + bper[i - 1]);
-            }
-
-            for (int i = 0; i < k; ++i)
-            {
-                swap(permutation[i], permutation[i + 1]);
-            }
-
-            bs.erase(0);
-
-            while (!bs.empty())
-            {
-                as.erase(*bs.begin());
-                bs.erase(bs.begin());
-            }
-
-            if (as.find(1) != as.end())
-            {
-                as.erase(1);
-            }
-
-            for (int i = n - k, j = n - 1; i < j; ++i, --j)
-                swap(permutation[i], permutation[j]);
-
-            for (int i = 1; i < n; i++)
-            {
-                if (prefSum == bper[i])
-                {
-                    per[i] = (*as.begin());
-                    as.erase(as.begin());
-                }
-                else
-                {
-                    prefSum = bper[i];
-                    per[i] = 1 + prefSum;
-                }
-            }
-
-            for (int i = 0; i < n && k > 0; i += 2, --k)
-                swap(permutation[i], permutation[i + 1]);
-
-            for (int i = 0; i < n; i++)
-            {
-                cout << per[i]+(permutation[i]*cnt) << " ";
-            }
-            cout << endl;
         }
     }
 
     return 0;
 }
-
-/*
-4
-366505800
-
-*/
--- Practice Question/800-2000/test1.cpp
+++ Practice Question/800-2000/test1.cpp
@@ -1,12 +1,96 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-int a[55], b[55];
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-    }
+#include <bits/stdc++.h> 
+using namespace std; 
+
+#define int long long 
+
+int findCount(vector<int> &frequency, int left, int right) 
+{ 
+    if(left == 0) 
+    { 
+        return frequency[right]; 
+    } 
+    return frequency[right] - frequency[left - 1]; 
+} 
+
+int sparseTable[1000001][20]; 
+  
+void buildSparseTable(int array[], int size) 
+{ 
+    for (int i = 0; i < size; i++) 
+        sparseTable[i][0] = array[i]; 
+  
+    for (int j = 1; j <= 20; j++) 
+        for (int i = 0; i + (1 << j) <= size; i++) 
+            sparseTable[i][j] = sparseTable[i][j - 1] + sparseTable[i + (1 << (j - 1))][j - 1]; 
+} 
+  
+int query(int left, int right) 
+{ 
+    int sum = 0; 
+    for (int j = 20; j >= 0; j--) 
+    { 
+        if ((1 << j) <= right - left + 1) 
+        { 
+            sum += sparseTable[left][j]; 
+            left += 1 << j; 
+        } 
+    } 
+    return sum; 
+} 
+
+signed main() 
+{ 
+    int testCases; 
+    cin >> testCases; 
+    while (testCases--) 
+    { 
+        int size, queries; 
+        cin >> size >> queries; 
+        int array[size]; 
+        for (int i = 0; i < size; i++) 
+        { 
+            cin >> array[i]; 
+        } 
+        vector<int> frequency(size, 0); 
+        for (int i = 0; i < size; i++) 
+        { 
+            if (i != 0) 
+            { 
+                frequency[i] = frequency[i - 1]; 
+            } 
+            if (array[i] == 1) 
+            { 
+                frequency[i]++; 
+            } 
+        } 
+         
+        buildSparseTable(array, size); 
+        while(queries--) 
+        { 
+            int left, right; 
+            cin >> left >> right; 
+            left--; 
+            right--; 
+            if(left == right) 
+            { 
+                cout << "NO" << endl; 
+                continue; 
+            } 
+            if(findCount(frequency, left, right) == 0) 
+            { 
+                cout << "YES" << endl; 
+                continue; 
+            } 
+            int count = findCount(frequency, left, right); 
+            int sumOfRest = query(left, right) - count; 
+            int countOfRest = right - left + 1 - count; 
+            if(count <= sumOfRest - countOfRest) 
+            { 
+                cout << "YES" << endl; 
+                continue; 
+            } 
+            cout << "NO" << endl; 
+        } 
+    } 
+    return 0; 
 }
