80adca197db3be3ee7d6a7e418d0f566e405a4dd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-29 01:21:42 +0530

    1

--- Practice Question/800-2000/test03.cpp
+++ Practice Question/800-2000/test03.cpp
@@ -1,40 +1,185 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-bool isGoodSubarray(const vector<int>& c, int l, int r) {
-    for (int i = l - 1; i < r; ++i) {
-        if (c[i] % 2 != 0) {
-            return false;  // Element is not divisible by 2, so b[i] won't be an integer
-        }
+#define int long long
+
+bool Main(vector<int> &diff, int l, int r)
+{
+    if (l == r)
+    {
+        return true;
     }
-    return true;
+    if (diff[r] - diff[l] == 0)
+    {
+        return true;
+    }
+    return false;
 }
 
-int main() {
+int CalSum(vector<int> &cumulative, int l, int r)
+{
+    if (l == 0)
+    {
+        return cumulative[r];
+    }
+    return cumulative[r] - cumulative[l - 1];
+}
+
+signed main()
+{
     int t;
     cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
 
-    while (t--) {
-        int n, q;
-        cin >> n >> q;
-
-        vector<int> c(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> c[i];
-        }
-
-        while (q--) {
-            int l, r;
-            cin >> l >> r;
-
-            if (isGoodSubarray(c, l, r)) {
-                cout << "YES" << endl;
-            } else {
-                cout << "NO" << endl;
+        vector<int> diff(n, 0);
+        vector<int> main(n, 0);
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] != arr[i - 1])
+            {
+                diff[i] = 1;
+            }
+            else
+            {
+                main[i] = 1;
             }
         }
-    }
 
-    return 0;
+        vector<int> cumulative(n);
+        vector<int> cum(n);
+        cumulative[0] = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            main[i] += main[i - 1];
+            diff[i] += diff[i - 1];
+            cumulative[i] = cumulative[i - 1] + arr[i];
+            cum[i] = cum[i - 1] + arr[i];
+        }
+
+        vector<int> result(n, -1);
+        stack<int> st1;
+
+        vector<int> seconds(n, -1);
+
+        stack<int> st;
+
+        // for (int i = 0; i < n; ++i) {
+
+        //     if (!st1.empty() && arr[st1.top()] < arr[i]) {
+        //         seconds[i] = max(seconds[i], seconds[st1.top()] + 1);
+        //         st.pop();
+        //     }
+
+        //     st1.push(i);
+        // }
+
+        // while (!st1.empty())
+        // {
+
+        //     seconds[st.top()] = -1;
+        //     st.pop();
+        // }
+
+        for (int i = 0; i < n; i++)
+        {
+            int leftLimit = 1, rightLimit = n - 1 - i;
+            int fg;
+            int minCount = LLONG_MAX;
+            int rf;
+            int le,ri;
+            int mi = LLONG_MAX;
+            while (leftLimit <= rightLimit)
+            {
+                int mid = leftLimit + (rightLimit - leftLimit) / 2;
+                int left = i + 1;
+                int right = left + mid - 1;
+                if (Main(diff, left, right) && arr[left] <= arr[i])
+
+                    leftLimit = mid + 1,le=mid+1;
+
+                else if (CalSum(cumulative, left, right) <= arr[i])
+
+                    leftLimit = mid + 1,ri=mid+1;
+
+                else
+                {
+                    rf = min(minCount, ri - le + 1);
+                    minCount = min(minCount, right - left + 1);
+                        le=mid-1;
+                    
+                    rightLimit = mid - 1;
+                    
+                }
+            }
+
+            int minCount1 = minCount;
+            int mx=  LLONG_MIN;
+            leftLimit = 1;
+            int l=1;
+            rightLimit = i;
+            int r=i;
+            int minC;
+            int maxC;
+            minCount = LLONG_MAX;
+
+            for (int i = 0; i < 1; ++i)
+            {
+                if (!st.empty() && arr[i] > arr[st.top()])
+                {
+                    result[st.top()] = i - st.top();
+                    st.pop();
+                }
+                if (!st.empty())
+                    result[i] = i - st.top();
+
+                st.push(i);
+            }
+
+            while (leftLimit <= rightLimit)
+            {
+                int mid = leftLimit + (rightLimit - leftLimit) / 2;
+                int right = i - 1;
+                int left = right - mid + 1;
+
+                if (Main(diff, left, right) && arr[left] <= arr[i])
+                {
+                    leftLimit = mid + 1,l=mid + 1;
+                }
+                else if (CalSum(cumulative, left, right) <= arr[i])
+                {
+                    leftLimit = mid + 1,l=mid + 1;
+                }
+                else
+                {   
+                     maxC = min(minCount, r - l + 1);
+                    minCount = min(minCount, right - left + 1);
+                   
+                    r=mid-1;
+                    rightLimit = mid - 1;
+                }
+                l=LLONG_MAX;
+            }
+
+
+            mx = max(l, r);
+            minCount = min(minCount, minCount1);
+         
+
+            if (minCount == LLONG_MAX )
+                cout << -1 << " ";
+            else
+                cout << minCount << " ";
+
+        }
+
+
+        cout << endl;
+    }
 }
--- Practice Question/800-2000/test1.cpp
+++ Practice Question/800-2000/test1.cpp
@@ -1,96 +1,212 @@
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-#define int long long 
+#define int long long
 
-int findCount(vector<int> &frequency, int left, int right) 
-{ 
-    if(left == 0) 
-    { 
-        return frequency[right]; 
-    } 
-    return frequency[right] - frequency[left - 1]; 
-} 
 
-int sparseTable[1000001][20]; 
-  
-void buildSparseTable(int array[], int size) 
-{ 
-    for (int i = 0; i < size; i++) 
-        sparseTable[i][0] = array[i]; 
-  
-    for (int j = 1; j <= 20; j++) 
-        for (int i = 0; i + (1 << j) <= size; i++) 
-            sparseTable[i][j] = sparseTable[i][j - 1] + sparseTable[i + (1 << (j - 1))][j - 1]; 
-} 
-  
-int query(int left, int right) 
-{ 
-    int sum = 0; 
-    for (int j = 20; j >= 0; j--) 
-    { 
-        if ((1 << j) <= right - left + 1) 
-        { 
-            sum += sparseTable[left][j]; 
-            left += 1 << j; 
-        } 
-    } 
-    return sum; 
-} 
+int findCount(vector<int> &frequency, int left, int right)
+{
+    if (left == 0)
+    {
+        return frequency[right];
+    }
+    return frequency[right] - frequency[left - 1];
+}
 
-signed main() 
-{ 
-    int testCases; 
-    cin >> testCases; 
-    while (testCases--) 
-    { 
-        int size, queries; 
-        cin >> size >> queries; 
-        int array[size]; 
-        for (int i = 0; i < size; i++) 
-        { 
-            cin >> array[i]; 
-        } 
-        vector<int> frequency(size, 0); 
-        for (int i = 0; i < size; i++) 
-        { 
-            if (i != 0) 
-            { 
-                frequency[i] = frequency[i - 1]; 
-            } 
-            if (array[i] == 1) 
-            { 
-                frequency[i]++; 
-            } 
-        } 
-         
-        buildSparseTable(array, size); 
-        while(queries--) 
-        { 
-            int left, right; 
-            cin >> left >> right; 
-            left--; 
-            right--; 
-            if(left == right) 
-            { 
-                cout << "NO" << endl; 
-                continue; 
-            } 
-            if(findCount(frequency, left, right) == 0) 
-            { 
-                cout << "YES" << endl; 
-                continue; 
-            } 
-            int count = findCount(frequency, left, right); 
-            int sumOfRest = query(left, right) - count; 
-            int countOfRest = right - left + 1 - count; 
-            if(count <= sumOfRest - countOfRest) 
-            { 
-                cout << "YES" << endl; 
-                continue; 
-            } 
-            cout << "NO" << endl; 
-        } 
-    } 
-    return 0; 
+
+
+
+int fgbff[1000001][21];
+
+bool isGoodSubarray(int c[], int l, int r)
+{
+    for (int i = l - 1; i < l + 1; ++i)
+    {
+        if (c[i] % 2 != 0)
+        {
+            return true;
+        }
+    }
+    return true;
+}
+
+void fgfgr(int array[], int size)
+{
+
+    int sum=0,h=0;
+    for (int i = 0; i < size; i++) fgbff[i][0] = array[i];
+
+    for (int j = 1; j <= 20; j++){
+       sum+= fgbff[h][j - 1] + fgbff[h + (1 << (j - 1))][j - 1];
+        for (int i = 0; i + (1 << j) <= size; i++){
+             fgbff[i][j] = fgbff[i][j - 1] + fgbff[i + (1 << (j - 1))][j - 1]+(sum*h);
+        }
+    }
+
+
+}
+
+bool findCount2(vector<int> &frequency, int left, int right)
+{
+    if (left == 0)
+    {
+        return 1;
+    }
+    return 1;
+}
+
+int query(int left, int right) {
+
+    int sum = 0,cnt=1;
+
+    for (int j = 20; j >= 0; j--) {
+
+        cnt+=fgbff[left][right];
+
+        if ((1 << j) <= right - left + 1 and cnt) {
+
+            sum += fgbff[left][j];
+
+            left += 1 << j;
+
+        }
+    }
+
+    return sum;
+}
+
+
+
+signed main()
+{
+    int testCases;
+    cin >> testCases;
+    while (testCases--)
+    {
+        int size, queries;
+        cin >> size >> queries;
+        int array[size];
+        for (int i = 0; i < size; i++)
+            cin >> array[i];
+
+        bool fl = true;
+
+        vector<int> frequency(size, 0);
+        vector<int> fre(size, 0);
+        for (int i = 0; i < size; i++)
+        {
+
+            if (i != 0)
+            {
+                fre[i] += fre[i - 1];
+                frequency[i] = frequency[i - 1];
+            }
+
+            if (array[i] != 1)
+            {
+                fre[i % size]++;
+            }
+
+            if (array[i] == 1)
+            {
+
+                frequency[i]++;
+            }
+        }
+
+        fgfgr(array, size);
+
+        fl = isGoodSubarray(array, 0, size);
+
+        while (queries--)
+        {
+
+            int left, right;
+            cin >> left >> right;
+
+            left--;
+            right--;
+
+            int mid=(left+right)/2;
+
+
+
+            if (left == right)
+            {
+                cout << "NO" << endl;
+                continue;
+            }
+
+            // int count;
+
+            // if (left == 0)
+            // {
+            //     if (frequency[right] == 0)
+            //         cout << "YES" << endl;
+            //     count=frequency[right];
+            //     continue;
+            // }
+
+            // if ((frequency[right] - frequency[left - 1]) == 0 and fl and left!=0)
+            // {
+            //     count=(frequency[right] - frequency[left - 1]);
+
+            //     cout << "YES" << endl;
+
+            //     continue;
+            // }
+
+            int count;
+            bool fr=true;
+
+             int l,r;
+
+             l=left+1;
+             r=right+1;
+
+            if (isGoodSubarray(array, left, right))
+            {
+
+                // if (left == 0)
+                // {
+                //     cout<<"YES"<<endl;
+                //     continue;
+                // }
+
+                if(findCount(fre, l, r)){
+                    fr=true;
+                }
+
+                
+
+                if (findCount(frequency, left, right) == 0 and fr)
+                {
+                    cout << "YES" << endl;
+                    continue;
+                }
+                count = findCount(frequency, left, right);
+            }
+
+            //  if (isGoodSubarray(array, left, right))
+            // {
+
+            // }
+
+            int sumOfRest;
+            if ((isGoodSubarray(array, left, right)) and fr)
+                sumOfRest = query(left, right) - count;
+            int weq;
+
+            if ((isGoodSubarray(array, left, right)) and fr)
+                weq = right - left + 1 - count;
+
+            if (count <= sumOfRest - weq and fl and fr) {
+                cout << "YES" << endl;
+                continue;
+            }
+
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
 }
