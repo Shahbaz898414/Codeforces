dd518b683959a7cf7605b0622712fd9db6c5563f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-29 20:14:36 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -5,6 +5,13 @@ using namespace std;
 #define endl "\n"
 #define mod 1000000007
 
+int find_position_of_1(int n)
+{
+
+    int position_of_1 = pow(2, floor(log2(n)));
+    return position_of_1;
+}
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
@@ -13,9 +20,12 @@ int32_t main()
     cin >> testCases;
     while (testCases--)
     {
-        int x,y;cin>>x>>y;
+        int n;
+        cin >> n;
 
-        
+        int position_of_1 = pow(2, floor(log2(n)));
+
+        cout << position_of_1 << endl;
     }
     return 0;
 }
