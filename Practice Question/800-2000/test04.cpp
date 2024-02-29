053209a0979d20db6dcfd226fe8af96328ba758d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-01 01:09:37 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -1,30 +1,37 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 #define ll long long
 #define endl "\n"
 #define mod 1000000007
 
-int32_t main()
-{
+
+
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int testCases;
-    cin >> testCases;
-    while (testCases--)
-    {
-        int n, k;
-        ll ans;
-        cin >> n >> k ;
+    int t; cin >> t;
+    while (t--) {
+
+        int n; ll x;
+        cin >> n >> x;
+        int flag = 0;
+        for (int i = 1;i<=n;i++) {
+            // cout<<x%i<<" "<<n-x/i+1<<endl;
+            if (x%i==0 && n-x/i+1>=i) {
+                flag = 1;
+                break;
+            }
+        }
+
+
+        if (flag==1) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
        
     }
     return 0;
 }
 
-
-/*
-
-
-The intution behind this problem is min of the subsegment lie between 1 to n. So its just the length that have to be seen. For 1 possible length are 1 to n. For 2 possible length are 1 to n-1. Similarly for number i possible lengths are 1 to n-(i-1). So now just we have fix the minimun number and check if it divides the required weight. If it divides then wether the lenght needed is in required region or not. So we checked if x%i == 0 if yes then it will be the minimum number of that subsegment. Then to find length we did x/i and checked if its possible. Finally we update our bool if both the conditions are met and get out of for loop.
-
-*/
