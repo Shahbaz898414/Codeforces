3ca3509b64dcf5231abecc340bf8c054ac8b0632
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-29 23:51:00 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define int long long
+#define ll long long
 #define endl "\n"
 #define mod 1000000007
-
-
-
 
 int32_t main()
 {
@@ -14,23 +11,45 @@ int32_t main()
     cin.tie(NULL);
     int testCases;
     cin >> testCases;
-    while (testCases--) {
-        int n,k;
-        int ans;
-
-        cin>>n>>k>>ans;
-
-        vector<int>  arr(n);
-
-        for (int i = 0; i < n; i++)
-        {
-            /* code */
-        }
+    while (testCases--)
+    {
+        int n, k;
+        ll ans;
+        cin >> n >> k >> ans;
+        vector<ll> v(n);
         
-      
-
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        
+        string s;
+        cin >> s;
+        if (k == 0)
+        {
+            cout << ans << endl;
+            continue;
+        }
+        if (s == "AND")
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ans = (ans & v[i]);
+            }
+        }
+        else if (s == "OR")
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ans = (ans | v[i]);
+            }
+        }
+        else if (k % 2 != 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ans = (ans ^ v[i]);
+            }
+        }
+        cout << ans << endl;
     }
     return 0;
 }
-
-
