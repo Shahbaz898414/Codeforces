b93cb0f7e9ca142fef8b2564220d361276422e4c
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-01 01:39:29 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -7,33 +7,38 @@ using namespace std;
 
 
 int32_t main() {
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t; cin >> t;
     while (t--) {
 
         int n; ll x;
+
+
         // 
+
+
+        cin >> n ;
+
+
+       vector<ll>  arr(n);
+
+       for (ll i = 0; i < n; i++)
+       {
+        /* code */
+        cin>>arr[i];
+       }
+       
         
-        cin >> n >> x;
-        int flag = 0;
-        for (int i = 1;i<=n;i++) {
-            // cout<<x%i<<" "<<n-x/i+1<<endl;
-            if (x%i==0 && n-x/i+1>=i) {
-                flag = 1;
-                break;
-            }
-        }
-
-
-        if (flag==1) {
-            cout << "YES" << endl;
-        }
-        else {
-            cout << "NO" << endl;
-        }
        
     }
     return 0;
 }
 
+
+/*
+
+The question statement is easy to understand. We need to replace all ci's in s with given pi's. So intitally we take a map to store c's and p's. Then we traverse through the string and find search in map if s[i] exists. If it exists then we replace that s[i] by p that is given bt mp[s[i]]. The tricky part here comes with the removal of last zeros. So we will remove last zeros only if we get a decimal in the modified string. So we need a bool decimal to tell us if we can remove las zeros. After removing starting and ending zeros we can output the final answer.
+
+*/
