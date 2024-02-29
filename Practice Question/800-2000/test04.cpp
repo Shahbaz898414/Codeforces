4c17aa854a8237e3f1c47c0da5b335eb1c8292ec
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-02-29 21:56:38 +0530

    1

--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
-#include <iostream>
+
 using namespace std;
 #define int long long
 #define endl "\n"
 #define mod 1000000007
 
-int find_position_of_1(int n)
-{
 
-    int position_of_1 = pow(2, floor(log2(n)));
-    return position_of_1;
-}
+
 
 int32_t main()
 {
@@ -18,14 +14,21 @@ int32_t main()
     cin.tie(NULL);
     int testCases;
     cin >> testCases;
-    while (testCases--)
-    {
-        int n;
-        cin >> n;
+    while (testCases--) {
+      int n;cin>>n;
 
-        int position_of_1 = pow(2, floor(log2(n)));
 
-        cout << position_of_1 << endl;
+      vector<int>  arr(n);
+
+      for (int i = 0; i < n; i++)
+      {
+        /* code */
+        cin>>arr[i];
+      }
+      
+
     }
     return 0;
 }
+
+
--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -1,59 +1,96 @@
-#include <bits/stdc++.h>
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-const long long mod = 1000000007;
+#define int long long
 
-long long permulation(int n)
-{
-    return (long long)n * (n + 1) / 2;
-}
-
-int main()
+signed main()
 {
     int t;
-    cin >> t;
-
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        int n, m;
-        cin >> n >> m;
+        int n;
+        cin>>n;
+        string s[2];
+        cin>>s[0]>>s[1];
 
-        int u = (n + 1) / 2;
-        int v = (m + 1) / 2;
+        int dp[2][n];
+        int pos=n;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[0][i]=='0')
+            {
+                pos=i;
+            }
+            dp[0][i]=pos;
+        }
+        pos=n;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[1][i]=='0')
+            {
+                pos=i;
+            }
+            dp[1][i]=pos;
+        }
 
-        int cnt = 0;
-
-        int ght = max({u - 1, v - 1, n - u, m - v});
-        // int mi = max({(u - 1) + (v - 1), (v - 1) + (m - v), n - u + ((v - 1)), m - v});
-
-        int r = min(1 + ght, n);
-
-        int l = max(n - ght, 1);
-        int rv = min(1 + ght, m);
-
-
-        // int fg = max(n - ght, 1);
-        // int bv = max(m - ght, 1);
-        // int tr = min(1 + ght, m);
-        // int re = max(m - ght, 1);
-
-        int lv = max(m - ght, 1);
-
-        // long long c = (permulation(fg) - permulation(bv - 1));
-        long long a = permulation(r) - permulation(l - 1);
-        long long b = permulation(rv) - permulation(lv - 1);
-        // long long f = (permulation(tr) - permulation(re - 1));
-
-        // long long
-
-        a %= mod;
-        // c %= mod;
-        // f %= mod;
-        b %= mod;
-
-        cout << ((a * b) % mod) << endl;
+        string ans="";
+        pos=0;
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(s[pos][i]);
+            if(i==n-1)
+            {
+                if(pos==0)
+                {
+                    ans.push_back(s[1][i]);
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            if(pos==0)
+            {
+                if(dp[0][i+1]>dp[1][i]+1)
+                {
+                    pos=1;
+                    i--;
+                }
+            }
+        }
+        vector<int> hue(n,0);
+        for(int i=n;i>=1;i--)
+        {
+            if(ans[i]==s[1][i-1])
+            {
+                hue[i-1]=1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        int val=0;
+        bool flag=false;
+        for(int i=0;i<n;i++)
+        {
+            if(ans[i]==s[0][i])
+            {
+                flag=true;
+                if(hue[i])
+                {
+                    val++;
+                }
+               
+            }
+            else if(flag)
+            {
+                break;
+            }
+        }
+        cout<<ans<<endl<<val<<endl;
     }
-
-    return 0;
 }
