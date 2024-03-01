33e2af1430c9dbd8f4916a927d18cf6b0fa04103
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-02 02:20:36 +0530

    1

--- Practice Question/800-2000/E. Klever Permutation.cpp
+++ Practice Question/800-2000/E. Klever Permutation.cpp
@@ -14,46 +14,52 @@ int32_t main()
   while (t--)
   {
 
-    ll n, k;
-    cin >> n >> k;
-
-    vector<ll> arr;
-    ll x = n - k;
-    ll b = x / 2;
-    ll a = (x / 2);
-
-    arr.push_back(1);
-    arr.push_back(n);
-
-    int k2 = k / 2;
-
-    for (int i = 1; i < k / 2; i++)
-    {
-      int adiff, bdiff;
-      if (a % k2 != 0)
-      {
-        adiff = (a / k2) + 1;
-        a -= ((a / k2) + 1);
-      }
-      else
-      {
-        adiff = (a / k2);
-        a -= ((a / k2));
-      }
-      if (b % k2 != 0)
-      {
-        bdiff = (b / k2) + 1;
-        b -= ((b / k2) + 1);
-      }
-      else
-      {
-        bdiff = (b / k2);
-        b -= ((b / k2));
-      }
-      arr.push_back(arr[arr.size() - 2] + adiff + 1);
-      arr.push_back(arr[arr.size() - 2] - (bdiff + 1));
-      k2--;
+     ll n,k;
+    cin>>n>>k;
+    vector<ll> ans;
+    ll x=n-k;
+    int a=(x+1)/2;
+    int b=x/2;
+    ans.push_back(1);
+    ans.push_back(n);
+    int k2=k/2;
+    for(int i=1;i<k/2;i++){
+        int adiff,bdiff;
+        if(a%k2!=0){
+            adiff=(a/k2)+1;
+            a-=((a/k2)+1);
+        }
+        else{
+            adiff=(a/k2);
+            a-=((a/k2));
+        }
+        if(b%k2!=0){
+            bdiff=(b/k2)+1;
+            b-=((b/k2)+1);
+        }
+        else{
+            bdiff=(b/k2);
+            b-=((b/k2));
+        }
+        ans.push_back(ans[ans.size()-2]+adiff+1);
+        ans.push_back(ans[ans.size()-2]-(bdiff+1));
+        k2--;
     }
+    for(int i=k;i<n;i++){
+        if(i%2==0)
+          ans.push_back(ans[i-k]+1);
+        else
+          ans.push_back(ans[i-k]-1);
+    }
+
+    for(auto it:ans) {
+
+      cout<<it<<" ";
+
+    }
+
+    cout<<endl;
+
   }
   return 0;
 }
