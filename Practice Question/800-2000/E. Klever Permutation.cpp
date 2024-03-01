9f822868345c8f1c5e8b99302ad41197e3845093
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-02 00:45:18 +0530

    1

--- Practice Question/800-2000/E. Klever Permutation.cpp
+++ Practice Question/800-2000/E. Klever Permutation.cpp
@@ -4,27 +4,56 @@ using namespace std;
 #define endl "\n"
 #define mod 1000000007
 
+int32_t main()
+{
 
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+  {
 
-int32_t main() {
+    ll n, k;
+    cin >> n >> k;
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t; cin >> t;
-    while (t--) {
+    vector<ll> arr;
+    ll x = n - k;
+    ll b = x / 2;
+    ll a = (x / 2);
 
-        int n,k;cin>>n>>k;
+    arr.push_back(1);
+    arr.push_back(n);
 
-        vector<int>  arr;
+    int k2 = k / 2;
 
-
-
-
-
-       
-        
-       
+    for (int i = 1; i < k / 2; i++)
+    {
+      int adiff, bdiff;
+      if (a % k2 != 0)
+      {
+        adiff = (a / k2) + 1;
+        a -= ((a / k2) + 1);
+      }
+      else
+      {
+        adiff = (a / k2);
+        a -= ((a / k2));
+      }
+      if (b % k2 != 0)
+      {
+        bdiff = (b / k2) + 1;
+        b -= ((b / k2) + 1);
+      }
+      else
+      {
+        bdiff = (b / k2);
+        b -= ((b / k2));
+      }
+      arr.push_back(arr[arr.size() - 2] + adiff + 1);
+      arr.push_back(arr[arr.size() - 2] - (bdiff + 1));
+      k2--;
     }
-    return 0;
+  }
+  return 0;
 }
-
