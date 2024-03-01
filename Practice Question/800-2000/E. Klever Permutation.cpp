2f9345999164111b9a0bfb2a4ad71f2b9f4a8331
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-02 00:07:26 +0530

    1

---
+++ Practice Question/800-2000/E. Klever Permutation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define mod 1000000007
+
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while (t--) {
+
+        int n,k;cin>>n>>k;
+
+        vector<int>  arr;
+
+
+
+
+
+       
+        
+       
+    }
+    return 0;
+}
+
--- Practice Question/800-2000/test04.cpp
+++ Practice Question/800-2000/test04.cpp
@@ -14,21 +14,32 @@ int32_t main() {
     while (t--) {
 
         int n; ll x;
-
-
-        // 
-
-
         cin >> n ;
+
+        ll pos=0,neg=0,ze=0;
 
 
        vector<ll>  arr(n);
 
-       for (ll i = 0; i < n; i++)
-       {
-        /* code */
-        cin>>arr[i];
+
+
+       for (ll i = 0; i < n; i++) {
+        
+            cin>>arr[i];
+            if(arr[i]<0) neg++;
+
+            if(arr[i]>0) pos++;
+
+            if(arr[i]==0) ze++;
        }
+
+       sort(arr.begin(),arr.end());
+
+
+
+
+
+
        
         
        
@@ -36,9 +47,3 @@ int32_t main() {
     return 0;
 }
 
-
-/*
-
-The question statement is easy to understand. We need to replace all ci's in s with given pi's. So intitally we take a map to store c's and p's. Then we traverse through the string and find search in map if s[i] exists. If it exists then we replace that s[i] by p that is given bt mp[s[i]]. The tricky part here comes with the removal of last zeros. So we will remove last zeros only if we get a decimal in the modified string. So we need a bool decimal to tell us if we can remove las zeros. After removing starting and ending zeros we can output the final answer.
-
-*/
