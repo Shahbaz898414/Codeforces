e877d91804c1e399658c594ce9a39202c8593f40
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-02 14:20:54 +0530

    1

--- Practice Question/800-2000/B. Yet Another Coin Problem.cpp
+++ Practice Question/800-2000/B. Yet Another Coin Problem.cpp
@@ -18,17 +18,41 @@ int32_t main()
 
     ll n;cin>>n;
 
-    vector<ll>  arr(n);
-
-    for (ll i = 0; i < n; i++) {
-      
-      cin>>arr[i];
-    }
-
-    sort(arr.begin(),arr.end());
-
-    cout<<2*(arr[n-1]-arr[0]+arr[n-2]-arr[1])<<endl;
+   
     
   }
   return 0;
 }
+
+
+// 17
+// 98
+// 15 6
+// 8
+
+
+/*
+
+
+
+ll n, res = 1e9;
+		cin >> n;
+		ll a[100], c[5] = {1, 3, 6, 10, 15};
+		a[0] = 0;
+		for (ll i = 1; i <= 100; i++) {
+			a[i] = 1e9;
+			for (ll j = 0; j < 5; j++) {
+				if (c[j] > i)
+					continue;
+				a[i] = min(a[i], a[i - c[j]] + 1);
+			}
+		}
+		for (ll i = 0; i < 5; i++) {
+			res = min(res, n / c[i] + a[n % c[i]]);
+			if (n < c[i])
+				continue;
+			res = min(res, n / c[i] - 1 + a[n % c[i] + c[i]]);
+		}
+		cout << res << "\n";
+
+*/
