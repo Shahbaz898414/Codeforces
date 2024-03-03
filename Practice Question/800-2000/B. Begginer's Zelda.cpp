cbc4a16e08416b3eed82c31d68c6f45cf28bddb4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-03 22:44:00 +0530

    1

---
+++ Practice Question/800-2000/B. Begginer's Zelda.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define mod 1000000007
+
+int32_t main()
+{
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+
+    ll n,m;
+		cin >> n;
+		
+
+   
+    
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
