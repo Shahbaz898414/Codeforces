eaad9bf641569e0d84a1bf5ebc41ed6f2f178e68
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-04 01:02:56 +0530

    1

--- Practice Question/800-2000/D2. Set To Max (Hard Version).cpp
+++ Practice Question/800-2000/D2. Set To Max (Hard Version).cpp
@@ -1,60 +1,188 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define endl "\n"
 #define mod 1000000007
+#define Time cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+
+#define pb push_back
+#define mp make_pair
+#define line cout << endl;
+#define ff first
+#define ss second
+#define vi vector<int>
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define fr(i, a, b) for (int i = a; i < b; i++)
+#define printv(v)                        \
+    for (int i = 0; i < (v.size()); i++) \
+    {                                    \
+        cout << v[i] << " ";             \
+    }                                    \
+    line;
+#define onesbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define tk(x) \
+    int x;    \
+    cin >> x;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+template <class T>
+void _print(T t)
+{
+    cerr << t;
+}
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(vector<vector<T>> v)
+{
+    cerr << "[\n";
+    for (int l = 0; l < v.size(); l++)
+    {
+        {
+            for (int k = 0; k < v[l].size(); k++)
+                cerr << v[l][k] << " ";
+        }
+        cerr << "\n";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+const long long inf = 1e18;
+const int MOD = 998244353;
+const int MAX = 1e6;
+
+bool isValid(string s)
+{
+    int len = s.size();
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+bool isregular(string t)
+{
+    int n = t.length(), co = 0;
+    fr(i, 0, n)
+    {
+        if (t[i] == ')')
+            co--;
+        else
+            co++;
+        if (co < 0)
+            return false;
+    }
+    if (co > 0)
+        return false;
+    return true;
+}
 
 int32_t main()
 {
 
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+    ll t;
+    cin >> t;
 
-  int t;
-  cin >> t;
+    while (t--) {
 
-  while (t--)
-  {
-
-    ll n; cin >> n;
+        ll n; cin >> n;
         vector<ll> a(n), b(n);
-        
+
         for (ll i = 0; i < n; i++) cin >> a[i];
         for (ll i = 0; i < n; i++) cin >> b[i];
-        
+
         deque<ll> dq;
-        
-        for (ll i = 0; i < n; i++) {
-            while (!dq.empty() && dq.front() > b[i]) dq.pop_front();
-            while (!dq.empty() && dq.back() < a[i]) dq.pop_back();
-            
+
+        for (ll i = 0; i < n; i++)
+        {
+            while (!dq.empty() && dq.front() > b[i])
+                dq.pop_front();
+            while (!dq.empty() && dq.back() < a[i])
+                dq.pop_back();
+
+                debug(a)
+
+                cout<<endl;
+                debug(b)
+                cout<<endl;
+
             dq.push_back(a[i]);
+
             a[i] = dq.front();
+
+
         }
-        
+
         dq.clear();
-        
-        for (ll i = n - 1; i >= 0; i--) {
-            while (!dq.empty() && dq.front() > b[i]) dq.pop_front();
-            while (!dq.empty() && dq.back() < a[i]) dq.pop_back();
-            
+
+        for (ll i = n - 1; i >= 0; i--)
+        {
+            while (!dq.empty() && dq.front() > b[i])
+                dq.pop_front();
+            while (!dq.empty() && dq.back() < a[i])
+                dq.pop_back();
+
             dq.push_back(a[i]);
             a[i] = dq.front();
         }
+
+
         if (a != b) cout << "NO" << endl;
-        else cout << "YES" << endl;    
-	
-    
-  }
-  return 0;
+        else cout << "YES" << endl;
+    }
 }
-
-
-
-
-
-
-
-
-
-
--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+#define ll long long
 #define mod 1000000007
 #define Time cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
 
@@ -13,91 +13,91 @@ using namespace std;
 #define no cout << "NO" << endl;
 #define yes cout << "YES" << endl;
 #define fr(i, a, b) for (int i = a; i < b; i++)
-#define printv(v)                      \
-  for (int i = 0; i < (v.size()); i++) \
-  {                                    \
-    cout << v[i] << " ";               \
-  }                                    \
-  line;
+#define printv(v)                        \
+    for (int i = 0; i < (v.size()); i++) \
+    {                                    \
+        cout << v[i] << " ";             \
+    }                                    \
+    line;
 #define onesbits(x) __builtin_popcountll(x)
 #define zerobits(x) __builtin_ctzll(x)
 #define sp(x, y) fixed << setprecision(y) << x
-#define w(x) \
-  int x;     \
-  cin >> x;  \
-  while (x--)
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
 #define tk(x) \
-  int x;      \
-  cin >> x;
+    int x;    \
+    cin >> x;
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
-#define debug(x)     \
-  cerr << #x << " "; \
-  _print(x);         \
-  cerr << endl;
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
 #else
 #define debug(x)
 #endif
 template <class T>
 void _print(T t)
 {
-  cerr << t;
+    cerr << t;
 }
 template <class T, class V>
 void _print(pair<T, V> p)
 {
-  cerr << "{";
-  _print(p.ff);
-  cerr << ",";
-  _print(p.ss);
-  cerr << "}";
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
 }
 template <class T>
 void _print(vector<T> v)
 {
-  cerr << "[ ";
-  for (T i : v)
-  {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
 }
 template <class T>
 void _print(vector<vector<T>> v)
 {
-  cerr << "[\n";
-  for (int l = 0; l < v.size(); l++)
-  {
+    cerr << "[\n";
+    for (int l = 0; l < v.size(); l++)
     {
-      for (int k = 0; k < v[l].size(); k++)
-        cerr << v[l][k] << " ";
+        {
+            for (int k = 0; k < v[l].size(); k++)
+                cerr << v[l][k] << " ";
+        }
+        cerr << "\n";
     }
-    cerr << "\n";
-  }
-  cerr << "]";
+    cerr << "]";
 }
 template <class T, class V>
 void _print(map<T, V> v)
 {
-  cerr << "[ ";
-  for (auto i : v)
-  {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
 }
 template <class T>
 void _print(set<T> v)
 {
-  cerr << "[ ";
-  for (T i : v)
-  {
-    _print(i);
-    cerr << " ";
-  }
-  cerr << "]";
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
 }
 
 const long long inf = 1e18;
@@ -106,18 +106,20 @@ const int MAX = 1e6;
 
 bool isValid(string s)
 {
-  int len = s.size();
-  for (int i = 0; i < len / 2; i++)
-  {
-    if (s[i] != s[len - 1 - i])
-      return false;
-  }
-  return true;
+    int len = s.size();
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - 1 - i])
+            return false;
+    }
+    return true;
 }
 
-bool isregular(string t) {
-   int n = t.length(), co = 0;
-    fr(i, 0, n) {
+bool isregular(string t)
+{
+    int n = t.length(), co = 0;
+    fr(i, 0, n)
+    {
         if (t[i] == ')')
             co--;
         else
@@ -130,33 +132,60 @@ bool isregular(string t) {
     return true;
 }
 
-                
+int32_t main()
+{
 
-int32_t main(){
+    ll t;
+    cin >> t;
 
-    int n,k,sum=0,i;cin>>n>>k;
-    // int a[n];
-    pair<int,int> a[n];
-    // vector<int> v;
-    for ( i = 0; i <n; i++) {
-      cin>>a[i].ff;
-      a[i].ss=i+1;
-    }
-
-    sort(a,a+n);
-
-    for ( i = 0; i <n; i++)
+    while (t--)
     {
-      k-=a[i].ff;
-      if(k<0)
-        break;
-    }
+        ll n;
+        cin >> n;
+        vector<ll> a(n), b(n);
 
-    cout<<i<<endl;
+        for (ll i = 0; i < n; i++)
+            cin >> a[i];
+        for (ll i = 0; i < n; i++)
+            cin >> b[i];
 
-    while(i--){
-      cout<<a[i].ss<<" ";
+        deque<ll> dq;
+
+        for (ll i = 0; i < n; i++)
+        {
+            while (!dq.empty() && dq.front() > b[i])
+                dq.pop_front();
+            while (!dq.empty() && dq.back() < a[i])
+                dq.pop_back();
+
+                debug(a)
+
+                cout<<endl;
+                debug(b)
+                cout<<endl;
+
+            dq.push_back(a[i]);
+
+            a[i] = dq.front();
+
+
+        }
+
+        dq.clear();
+
+        for (ll i = n - 1; i >= 0; i--)
+        {
+            while (!dq.empty() && dq.front() > b[i])
+                dq.pop_front();
+            while (!dq.empty() && dq.back() < a[i])
+                dq.pop_back();
+
+            dq.push_back(a[i]);
+            a[i] = dq.front();
+        }
+        if (a != b)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
     }
-    
-    return 0;
 }
