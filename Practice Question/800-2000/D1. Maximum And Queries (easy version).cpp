dc820520ba3f1d1f9d42503ee2aaa2c10365b145
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-04 14:54:51 +0530

    1

--- Practice Question/800-2000/D1. Maximum And Queries (easy version).cpp
+++ Practice Question/800-2000/D1. Maximum And Queries (easy version).cpp
@@ -135,53 +135,5 @@ bool isregular(string t)
 int32_t main()
 {
 
-    ll t;
-    cin >> t;
-
-    while (t--)
-    {
-
-        ll n;
-        cin >> n;
-        vector<ll> a(n), b(n);
-
-        for (ll i = 0; i < n; i++)
-            cin >> a[i];
-        for (ll i = 0; i < n; i++)
-            cin >> b[i];
-
-        deque<ll> dq;
-
-        for (ll i = 0; i < n; i++)
-        {
-
-            while (!dq.empty() && dq.front() > b[i])
-                dq.pop_front();
-            while (!dq.empty() && dq.back() < a[i])
-                dq.pop_back();
-
-            dq.push_back(a[i]);
-
-            a[i] = dq.front();
-        }
-
-        dq.clear();
-
-        for (ll i = n - 1; i >= 0; i--)
-        {
-            while (!dq.empty() && dq.front() > b[i])
-                dq.pop_front();
-            while (!dq.empty() && dq.back() < a[i])
-                dq.pop_back();
-
-            dq.push_back(a[i]);
-            a[i] = dq.front();
-        }
-
-        if (a != b)
-            cout << "NO" << endl;
-
-        else
-            cout << "YES" << endl;
-    }
+  
 }
