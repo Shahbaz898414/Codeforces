949caf06f450b3decfd7041577da7b583f47c8e7
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-04 23:17:40 +0530

    1

--- Practice Question/800-2000/D1. Maximum And Queries (easy version).cpp
+++ Practice Question/800-2000/D1. Maximum And Queries (easy version).cpp
@@ -13,91 +13,91 @@ using namespace std;
 #define no cout << "NO" << endl;
 #define yes cout << "YES" << endl;
 #define fr(i, a, b) for (int i = a; i < b; i++)
-#define printv(v)                        \
-    for (int i = 0; i < (v.size()); i++) \
-    {                                    \
-        cout << v[i] << " ";             \
-    }                                    \
-    line;
+#define printv(v)                      \
+  for (int i = 0; i < (v.size()); i++) \
+  {                                    \
+    cout << v[i] << " ";               \
+  }                                    \
+  line;
 #define onesbits(x) __builtin_popcountll(x)
 #define zerobits(x) __builtin_ctzll(x)
 #define sp(x, y) fixed << setprecision(y) << x
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
+#define w(x) \
+  int x;     \
+  cin >> x;  \
+  while (x--)
 #define tk(x) \
-    int x;    \
-    cin >> x;
+  int x;      \
+  cin >> x;
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 #ifndef ONLINE_JUDGE
-#define debug(x)       \
-    cerr << #x << " "; \
-    _print(x);         \
-    cerr << endl;
+#define debug(x)     \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
 #else
 #define debug(x)
 #endif
 template <class T>
 void _print(T t)
 {
-    cerr << t;
+  cerr << t;
 }
 template <class T, class V>
 void _print(pair<T, V> p)
 {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
 }
 template <class T>
 void _print(vector<T> v)
 {
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 template <class T>
 void _print(vector<vector<T>> v)
 {
-    cerr << "[\n";
-    for (int l = 0; l < v.size(); l++)
+  cerr << "[\n";
+  for (int l = 0; l < v.size(); l++)
+  {
     {
-        {
-            for (int k = 0; k < v[l].size(); k++)
-                cerr << v[l][k] << " ";
-        }
-        cerr << "\n";
+      for (int k = 0; k < v[l].size(); k++)
+        cerr << v[l][k] << " ";
     }
-    cerr << "]";
+    cerr << "\n";
+  }
+  cerr << "]";
 }
 template <class T, class V>
 void _print(map<T, V> v)
 {
-    cerr << "[ ";
-    for (auto i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (auto i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 template <class T>
 void _print(set<T> v)
 {
-    cerr << "[ ";
-    for (T i : v)
-    {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 
 const long long inf = 1e18;
@@ -106,34 +106,70 @@ const int MAX = 1e6;
 
 bool isValid(string s)
 {
-    int len = s.size();
-    for (int i = 0; i < len / 2; i++)
-    {
-        if (s[i] != s[len - 1 - i])
-            return false;
-    }
-    return true;
+  int len = s.size();
+  for (int i = 0; i < len / 2; i++)
+  {
+    if (s[i] != s[len - 1 - i])
+      return false;
+  }
+  return true;
 }
 
 bool isregular(string t)
 {
-    int n = t.length(), co = 0;
-    fr(i, 0, n)
-    {
-        if (t[i] == ')')
-            co--;
-        else
-            co++;
-        if (co < 0)
-            return false;
-    }
-    if (co > 0)
-        return false;
-    return true;
+  int n = t.length(), co = 0;
+  fr(i, 0, n)
+  {
+    if (t[i] == ')')
+      co--;
+    else
+      co++;
+    if (co < 0)
+      return false;
+  }
+  if (co > 0)
+    return false;
+  return true;
 }
 
-int32_t main()
-{
+int32_t main() {
+
+  int n, q;cin >> n >> q;
+
+  vector<ll> a0(n);
+
+  for (ll &x : a0) cin >> x;
 
   
+  for (++q; --q;)
+  {
+    vector<ll> a = a0;
+    ll k;
+    cin >> k;
+    ll ans = 0, p = (1ll) << 60;
+    for (int bit = 60; bit >= 0; --bit)
+    {
+      ll necess = 0;
+      for (ll &x : a)
+      {
+        necess += max(p - x, 0ll);
+        if (necess > k)
+          break;
+      }
+      if (necess <= k)
+      {
+        ans += p;
+        k -= necess;
+        for (ll &x : a)
+          x = max(x, p);
+      }
+      for (ll &x : a)
+        x %= p;
+      p >>= 1;
+    }
+    
+    cout << ans << endl;
+  }
+
+
 }
