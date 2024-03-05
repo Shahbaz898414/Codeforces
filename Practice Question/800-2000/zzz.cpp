b3a103b085537b18d81d4d81e230d9921e9ef7ab
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-05 20:18:21 +0530

    1

--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -5,42 +5,26 @@ using namespace std;
 int32_t main()
 {
 
-  int n, q;
-  cin >> n >> q;
+ ll t;cin>>t;
 
-  vector<ll> a0(n);
+ while(t--){
 
-  for (ll &x : a0)
-    cin >> x;
+ }
 
-  for (++q; --q;)
-  {
 
-    vector<ll> a = a0;
-    ll k;
-    cin >> k;
-    ll ans = 0, p = (1ll) << 60;
-    for (int bit = 60; bit >= 0; --bit)
-    {
-      ll necess = 0;
-      for (ll &x : a)
-      {
-        necess += max(p - x, 0ll);
-        if (necess > k)
-          break;
-      }
-      if (necess <= k)
-      {
-        ans += p;
-        k -= necess;
-        for (ll &x : a)
-          x = max(x, p);
-      }
-      for (ll &x : a)
-        x %= p;
-      p >>= 1;
-    }
-
-    cout << ans << endl;
-  }
 }
+
+
+/*
+
+Initially, we set all di = 230 - 1 (all bits on).
+You can through every i,j such that i j and do ai & = Mi,j and aj& = Mi,j-
+Then we check if Mi.j = ai |aj for all pairs. If this holds you found the array else the answer is NO.
+
+Proof:
+
+Initially, all elements have all their bits set on and we remove only the bits that affect our answer. If Mi.j doesn't have a specific bit then
+definitely neither ai nor aj should have it. If Mi,j has a specific bit on then we don't have to remove anything (in the end we want at
+least one of i and aj to have the bit on).
+
+*/
