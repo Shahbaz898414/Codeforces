2fa1e41c2c59de68035894da4d71b297cd2c8c35
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-06 13:10:34 +0530

    1

--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -15,20 +15,3 @@ int32_t main()
 }
 
 
-/*
-
-2kg chane ki daal
-1 packet namak
-1 packet saraf XL
-weemBar soap
-chura biscuit
-1kg tuwar
-1/2 kg laal masur
-5kg tel
-1kg chana
-1kg watana
-100gm haldi
-1/4 jira
-
-
-*/
