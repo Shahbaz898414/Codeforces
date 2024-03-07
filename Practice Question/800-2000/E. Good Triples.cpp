b5849d9126a330418587dfae5b4565efb3638586
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-07 20:07:29 +0530

    1

--- Practice Question/800-2000/E. Good Triples.cpp
+++ Practice Question/800-2000/E. Good Triples.cpp
@@ -48,3 +48,20 @@ int main()
 
   }
 }
+
+
+
+/*
+
+1kg chicken
+1/2kg tamator
+10 ka sajira
+10 ka khas khas
+10 ka hara masala
+1/4 kakri and gajar
+10 ka goad
+10 ka misri
+10 ka khopra
+10 ka tal makhana 
+
+*/
