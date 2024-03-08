35d011fde3246c4bfe9c53d245ccead45f11ff0c
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-08 21:37:33 +0530

    1

--- Practice Question/800-2000/test05.cpp
+++ Practice Question/800-2000/test05.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,22 +6,37 @@ using namespace std;
 
 signed main()
 {
+
     int t;
-    cin>>t;
-    while(t--) {
-      int n,k;cin>>n>>k;
+    cin >> t;
+    while (t--)
+    {
 
-      vector<int>  vec(n);
-
-      for (int i = 0; i < n; i++)
-      {
-        
-        cin>>vec[i];
-      }
-      
-
-    
-
-
+        int n, k;
+        cin >> n >> k;
+        int ans = INT_MAX;
+        int eve = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int num;
+            cin >> num;
+            if (num % 2 == 0)
+                eve++;
+            if (num % k != 0)
+            {
+                int a = num % k;
+                ans = min(ans, k - a);
+            }
+            else
+                ans = 0;
+        }
+        if (k == 4 && eve >= 2 || ans == 0)
+            cout << 0 << endl;
+        else if (k == 4 && eve == 1)
+            cout << 1 << endl;
+        else if (k == 4)
+            cout << min(2ll, ans) << endl;
+        else
+            cout << ans << endl;
     }
 }
