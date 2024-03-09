238b3991717d3f5a2bb2f8cf7e69350674b609b2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-09 13:46:18 +0530

    1

--- Practice Question/800-2000/D. Yet Another Monster Fight.cpp
+++ Practice Question/800-2000/D. Yet Another Monster Fight.cpp
@@ -4,26 +4,40 @@ using namespace std;
 
 #define int long long
 
-
-
 signed main()
 {
 
-  int t;
-  cin >> t;
-  while (t--)
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto &it : a)
+    cin >> it;
+
+  vector<int> pref(n), suf(n);
+  for (int i = 0; i < n; ++i)
   {
-    int n;cin>>n;
-
-    vector<int>  arr(n);
-
-    for (int i = 0; i < n; i++)
-    {
-      /* code */
-      cin>>arr[i];
-    }
-    
-   
+    pref[i] = a[i] + (n - i - 1);
+    suf[i] = a[i] + i;
   }
-}
+  for (int i = 1; i < n; ++i)
+  {
+    pref[i] = max(pref[i], pref[i - 1]);
+  }
+  for (int i = n - 2; i >= 0; --i)
+  {
+    suf[i] = max(suf[i], suf[i + 1]);
+  }
 
+  int ans = 2e9;
+  for (int i = 0; i < n; ++i)
+  {
+    int cur = a[i];
+    if (i > 0)
+      cur = max(cur, pref[i - 1]);
+    if (i + 1 < n)
+      cur = max(cur, suf[i + 1]);
+    ans = min(ans, cur);
+  }
+
+  cout << ans << endl;
+}
