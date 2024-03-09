def906268a7d571306f667929366e3ef413a4de5
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-09 16:59:29 +0530

    1

--- Practice Question/800-2000/D. Yet Another Monster Fight.cpp
+++ Practice Question/800-2000/D. Yet Another Monster Fight.cpp
@@ -6,70 +6,59 @@ using namespace std;
 
 signed main()
 {
-
   int n;
   cin >> n;
-  vector<int> a(n);
-  for (auto &it : a)
-    cin >> it;
 
-  vector<int> pref(n), suf(n);
-  for (int i = 0; i < n; ++i)
+  vector<int> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  vector<int> pre(n, 2e9), suff(n, 2e9);
+
+  for (int i = 0; i < n; i++)
   {
-    pref[i] = a[i] + (n - i - 1);
-    suf[i] = a[i] + i;
+    if (i == 0)
+      pre[i] = a[i];
+    else
+      pre[i] = max(pre[i - 1] + 1, a[i]);
   }
-  for (int i = 1; i < n; ++i)
+
+  // for(int i=0;i<n;i++){
+  //   cout<<pre[i]<<" ";
+  // }
+  // cout<<endl;
+
+  for (int i = n - 1; i >= 0; i--)
   {
-    pref[i] = max(pref[i], pref[i - 1]);
+    if (i == n - 1)
+      suff[i] = a[i];
+    else
+      suff[i] = max(suff[i + 1] + 1, a[i]);
   }
-  for (int i = n - 2; i >= 0; --i)
-  {
-    suf[i] = max(suf[i], suf[i + 1]);
-  }
+
+
+  // for(int i=0;i<n;i++) {
+  //   cout<<suff[i]<<" ";
+  // }
+
+  // cout<<endl;
 
   int ans = 2e9;
-  for (int i = 0; i < n; ++i)
+
+  // i == 0
+  ans = min(ans, suff[0]);
+
+  // i == n - 1
+  ans = min(ans, pre[n - 1]);
+
+  for (int i = 1; i < n - 1; i++)
   {
-    int cur = a[i];
-    if (i > 0)
-      cur = max(cur, pref[i - 1]);
-    if (i + 1 < n)
-      cur = max(cur, suf[i + 1]);
-    ans = min(ans, cur);
+    int curr = 0;
+    curr = max(curr, a[i]);
+    curr = max(curr, i + 1 + suff[i + 1]);
+    curr = max(curr, (n - i) + pre[i - 1]);
+    ans = min(ans, curr);
   }
 
   cout << ans << endl;
 }
-
-
-
-
-
-
-/*
-
-#include<bits/stdc++.h>
-using namespace std;
-int n,num[300010],ans=2e9;
-int l[300010],r[300010];
-int main()
-{
-	cin>>n;
-	int temp1=0,temp2=0;
-	for(int i=1;i<=n;++i)
-	{
-		cin>>num[i];
-		l[i]=max(l[i-1],num[i]+(n-i));
-	}
-	for(int i=n;i>=1;--i)
-	{
-		r[i]=max(r[i+1],num[i]+i-1);
-		ans=min(ans,max(l[i-1],max(num[i],r[i+1])));
-	}
-	cout<<ans<<'\n';
-}
-
-
-
-*/
