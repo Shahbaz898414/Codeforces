7431dcc0715c887bed6a4dbb0125e8d5975380f8
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-16 17:56:02 +0530

    1

--- Practice Question/800-2000/B. Binary Path.cpp
+++ Practice Question/800-2000/B. Binary Path.cpp
@@ -3,18 +3,27 @@ using namespace std;
 #define ll long long
 
 
+int n;
+char ch[2][200010];
+int main()
+{
+	int i,j,_;
+	cin>>_;
+	while(_--){
+		cin>>n;
+		for(i=0;i<2;i++) for(j=1;j<=n;j++)
+        cin>>ch[i][j];
+			
+		int s=0;
+		for(i=1;i<n;i++){
+			if(ch[1][i]<ch[0][i+1])break;else cout<<ch[0][i];
+			if(ch[1][i]==ch[0][i+1])s++;else s=0;
+		}
 
-int main() {
-   ll t;cin>>t;
+		cout<<ch[0][i];
 
-   while(t--){
-      ll n;cin>>n;
+		for(;i<=n;i++)cout<<ch[1][i];
 
-    
-   }
-    return 0;
+		cout<<'\n'<<s+1<<'\n';
+	}
 }
-
-
-
-
