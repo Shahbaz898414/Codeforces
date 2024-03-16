998bff745c2a54c69422b8a41fce2235b8fe4d02
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-16 21:30:08 +0530

    1

--- Practice Question/800-2000/B. Binary Path.cpp
+++ Practice Question/800-2000/B. Binary Path.cpp
@@ -2,28 +2,41 @@
 using namespace std;
 #define ll long long
 
-
 int n;
 char ch[2][200010];
 int main()
 {
-	int i,j,_;
-	cin>>_;
-	while(_--){
-		cin>>n;
-		for(i=0;i<2;i++) for(j=1;j<=n;j++)
-        cin>>ch[i][j];
-			
-		int s=0;
-		for(i=1;i<n;i++){
-			if(ch[1][i]<ch[0][i+1])break;else cout<<ch[0][i];
-			if(ch[1][i]==ch[0][i+1])s++;else s=0;
-		}
+    int i, j, _;
+    cin >> _;
+    while (_--)
+    {
+        cin >> n;
+        for (i = 0; i < 2; i++)
+            for (j = 1; j <= n; j++)
+                cin >> ch[i][j];
 
-		cout<<ch[0][i];
+        int s = 0;
+        for (i = 1; i < n; i++)
+        {
+            if (ch[1][i] < ch[0][i + 1])
+                break;
+            else
+                cout << ch[0][i];
+            if (ch[1][i] == ch[0][i + 1])
+                s++;
+            else
+                s = 0;
+        }
 
-		for(;i<=n;i++)cout<<ch[1][i];
 
-		cout<<'\n'<<s+1<<'\n';
-	}
+        cout << ch[0][i];
+
+
+        for (; i <= n; i++)
+            cout << ch[1][i];
+
+
+        cout << '\n'
+             << s + 1 << '\n';
+    }
 }
