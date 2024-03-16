505635965292ca5b03d17dedd2932724c232e3b1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-16 23:07:19 +0530

    1

--- Practice Question/800-2000/B. Informatics in MAC.cpp
+++ Practice Question/800-2000/B. Informatics in MAC.cpp
@@ -2,27 +2,47 @@
 using namespace std;
 #define ll long long
 
+int main()
+{
 
-int main(){
+  ll t;
+  cin >> t;
 
+  while (t--)
+  {
 
-  ll t;cin>>t;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> cnt1(n + 1), cnt2(n + 1);
+    for (int i = 0; i < n; ++i)
+    {
+      cin >> a[i];
+      cnt2[a[i]]++;
+    }
+    int mex1 = 0, mex2 = 0;
+    while (cnt2[mex2])
+      ++mex2;
+    for (int i = 0; i < n; ++i)
+    {
+      cnt1[a[i]]++;
+      if (--cnt2[a[i]] == 0 && mex2 > a[i])
+      {
+        mex2 = a[i];
+      }
+      while (mex2 && !cnt2[mex2 - 1])
+        --mex2;
+      while (cnt1[mex1])
+        ++mex1;
 
-
-  while(t--){
-
-
-    ll n;cin>>n;
-
-
-    vector<ll>  arr(n);
-
-
-    for (ll i = 0; i < n; i++)  cin>>arr[i];
-    
-
-
-
-
+      if (mex1 == mex2)
+      {
+        cout << "2\n";
+        cout << 1 << " " << i + 1 << "\n";
+        cout << i + 2 << " " << n << "\n";
+        return;
+      }
+    }
+    cout << "-1\n";
   }
 }
