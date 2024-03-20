fbe6e3ef3ed330ad46d8372f92274a79a23fe289
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-21 02:09:59 +0530

    1

---
+++ Practice Question/800-2000/C. Turtle Fingers Count the Values of k.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[100010];
+int main()
+{
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+    sort(a, a + n);
+    if (a[0] != a[1])
+    {
+      cout << "YES\n";
+    }
+    else
+    {
+      bool PASS = 0;
+      for (int i = 1; i < n; i++)
+      {
+        if (a[i] % a[0] != 0)
+          PASS = 1;
+      }
+      if (PASS)
+        cout << "YES\n";
+      else
+        cout << "NO\n";
+    }
+  }
+}
