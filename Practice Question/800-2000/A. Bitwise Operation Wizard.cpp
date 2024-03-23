d313736f709fa5ffee123720a5f2baf5688463f6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-23 14:30:15 +0530

    1

--- Practice Question/800-2000/A. Bitwise Operation Wizard.cpp
+++ Practice Question/800-2000/A. Bitwise Operation Wizard.cpp
@@ -1,14 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[100010];
-int main()
-{
-  int t;
-  scanf("%d", &t);
-  while (t--)
-  {
+#define ll long long
 
-   
-  }
+void query(ll a, ll b, ll c, ll d)
+{
+  cout << "? " << a << " " << b << " " << c << " " << d << endl;
 }
 
+int main()
+{
+
+  ll t;
+  cin >> t;
+
+  while (t--)
+  {
+    ll n;
+    cin >> n;
+
+    if (n == 2)
+    {
+      cout << "! 0 1" << endl;
+      continue;
+    }
+
+    ll z = 0;
+    for (int i = 1; i < n; i++)
+    {
+      query(z, z, i, i);
+      string res;
+      cin >> res;
+      if (res == "<")
+      {
+        z = i;
+      }
+    }
+
+    ll mx = 0;
+    for (int i = 1; i < n; i++)
+    {
+      query(z, mx, z, i);
+      string res;
+      cin >> res;
+      if (res == "<")
+      {
+        mx = i;
+      }
+      else if (res == "=")
+      {
+        query(mx, mx, i, i);
+        string res;
+        cin >> res;
+        if (res == ">")
+        {
+          mx = i;
+        }
+      }
+    }
+    cout << "! " << mx << " " << z << endl;
+  }
+}
