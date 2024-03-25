be547dbef8956520509d9540f6bb1706db385e89
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-25 12:30:55 +0530

    1

--- Practice Question/800-2000/D. Tandem Repeats .cpp
+++ Practice Question/800-2000/D. Tandem Repeats .cpp
@@ -39,24 +39,48 @@ signed main()
 
  
             for(int i=0; i+len<n; i++) val[i] = (s[i]==s[i+len] || s[i]=='?' || s[i+len]=='?');
+
  
 
-            for(int i=1;i<n;i++) val[i] += val[i-1];
+            for(int i=1;i<n;i++) {
+              cout<<val[i-1]<<" ";
+              val[i] += val[i-1];
+            }
+
+            cout<<endl;
  
 
             if(val[len-1]==len)     ans = max(ans, 2*len);
  
 
             for(int i=len; i<n; i++) {
-                if(val[i]-val[i-len]==len) ans = max(ans, 2*len);
+
+                if( val[i] - val[i-len] == len ) ans = max(ans, 2*len);
             }
 
 
         }
- 
+
+
         cout<<ans<<"\n";
+
 
    }
 }
 
 
+
+/*
+
+
+Hi,
+
+I hope this message finds you well! I came across the Software Engineer ( https://talent.propelinc.com/jobs/Careers/26698000053870027/Fresher-2024-Software-Developer?source=CareerSite ) at propel. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+
+Best regards,
+Shahbaz Khan
+
+Resume Link :-
+https://drive.google.com/file/d/1dCI7ysrWVAlzk4qgI_PArpdtSatVI_oq/view?usp=sharing
+
+*/
