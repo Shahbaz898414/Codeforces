248dac6c7e34212865a92641ebb4abe2ad6bcfbb
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-27 01:29:48 +0530

    1

--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -32,29 +32,11 @@ signed main()
 
 /*
 
-One reason I want to become an MLH Fellow is that it will provide me with the chance to work on real-world projects and solutions. This opportunity to work on meaningful projects will allow me to build my skills and knowledge in software development, data processing, and other vital areas in computer science. Through this experience, I will be able to refine my coding abilities and learn about new technologies that I wouldn't have discovered otherwise.
+Hi,
 
-In addition to the technical skills, being an MLH fellow will also help me develop essential soft skills, such as communication, teamwork, and problem-solving. Working in a collaborative environment with other fellows and mentors will give me the chance to learn from others' perspectives and approaches while contributing my own ideas and solutions.
+I hope this message finds you well! I came across the Software Developers (https://www.zoho.com/careers/jobdetails/?job_id=2803000614929615) at Zoho. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
 
-X-> Another reason I want to become an MLH Fellow is the opportunity to be a part of a supportive and inclusive community. MLH fosters a welcoming environment where everyone's input is valued, enabling Fellows to connect with numerous industry professionals and expand their network.
-
-Furthermore, MLH provides its Fellows with resources and events to help build their careers beyond the program, such as industry-led workshops, job opportunities, and networking events. This program's benefits go beyond the three-month duration, providing a valuable foundation for our future in the technology industry.
-
-In conclusion, becoming an MLH Fellow is an opportunity that I am very passionate about because it will allow me to grow professionally and personally while working on meaningful projects and with an inclusive community of tech enthusiasts. It is a program that will undoubtedly enable me to hone my skills and broaden my experience, establishing a sustainable path for my future in the tech industry.
-
-
-
-
--------------------------------------------------------------------------------------------------------------------------
-
-
-
-One reason I want to become an MLH Fellow is the opportunity to be a part of a supportive and inclusive community. MLH provide a welcoming environment where everyone's input is valued, MLH Fellow enable to connect with different industry professionals and expand their network.
-
-Second reason I want to become an MLH Fellow Because it will provide me the chance to work on real-world projects and solutions.
-
-
-
-
+Best regards,
+Shahbaz Khan
 
 */
