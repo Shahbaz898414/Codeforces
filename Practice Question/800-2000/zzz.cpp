9cf33a918c976fb139f274c2d8eac4ede9986f02
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-03-28 15:38:34 +0530

    1

--- Practice Question/800-2000/zzz.cpp
+++ Practice Question/800-2000/zzz.cpp
@@ -25,22 +25,9 @@ signed main()
     int t;cin>>t;
 
     while(t--){
-        int a,b,m;cin>>a>>b>>m;
+       int a,b,c; cin>>a>>b>>c;
+		if((b+c)%3 > c) cout<<-1<<endl;
+		else cout<<(b+c+2)/3+a<<endl;
     }
 }
 
-
-
-/*
-
-#75dayschallenge
-#challenge
-#consistency
-#Cp 
-#AlgorithmExplained 
-#ProblemSolving
-#lessons
-#learning
-
-
-*/
