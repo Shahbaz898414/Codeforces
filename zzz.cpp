5b99e46343e9edd6ce0757e2f672aef3eec2aec0
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-02 15:31:37 +0530

    1

--- zzz.cpp
+++ zzz.cpp
@@ -144,8 +144,44 @@ Shahbaz Khan
 
 
 
+////////////////////////////////////////////////////////////////////////////////////
+
+
+class Solution:
+def numDecodings(self, s: str) -> int:
+		dp = { len(s) : 1}
+
+		def dfs(i):
+				if i in dp:
+				    return dp[i]
+				if s[i] == "0":
+				    return 0
+
+		    res = dfs(i+1)
+		    if (i + 1 < len(s) and (s[i] == "1" or
+		        s[i] == "2" and s[i + 1] in "0123456")) :
+		        res += dfs(i + 2)
+		    dp[i] = res
+		    return res
+
+		return dfs(e)
 
 
 
+*/
+
+
+/*
+
+
+Hi,
+
+I hope this message finds you well! I came across the Web Developer New Associate (https://www.accenture.com/in-en/careers/jobdetails?src=LINKEDINJP&id=AIOC-S01512838_en) at Accenture. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+
+Best regards,
+Shahbaz Khan
+
+Resume Link :-
+https://drive.google.com/file/d/1KtvsYeT5PYaj_gk8VTDHismkNZzeOu6z/view?usp=sharing
 
 */
