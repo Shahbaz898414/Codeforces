9b5612149d624454a42c386b3c25b4eada060f7f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-07 15:15:17 +0530

    1

--- C. Arrow Path.cpp
+++ C. Arrow Path.cpp
@@ -22,14 +22,113 @@ signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-   int t;cin>>t;
+    int t;
+    cin >> t;
 
-   while(t--){
-    int n;cin>>n;
+    while (t--)
+    {
+        int n;
+        cin >> n;
 
-    string s1,s2;cin>>s1>>s2;
+        vector<string> s(2);
+        cin >> s[0] >> s[1];
+        vector<vector<int>> pos(2, vector<int>(n));
+        pos[0][0] = 1;
+        queue<pair<int, int>> qp;
+        qp.push({0, 0});
+        while (qp.size()) {
+            auto e = qp.front();
+            int x = e.first;
+            int y = e.second;
+            qp.pop();
+            if (x - 1 >= 0)
+            {
+                if (s[x - 1][y] == '>' && !pos[x - 1][y + 1])
+                {
+                    // cout<<pos[x - 1][y + 1]<<" ";
+                    pos[x - 1][y + 1] = 1;
+                    qp.push({x - 1, y + 1});
+                }
+            }
+            if (x - 1 >= 0)
+            {
+                if (s[x - 1][y] == '<' && !pos[x - 1][y - 1])
+                {
+                    // cout<<pos[x - 1][y - 1]<<" ";
+                    pos[x - 1][y - 1] = 1;
+                    qp.push({x - 1, y - 1});
+                }
+            }
+            if (x + 1 <= 1)
+            {
+                if (s[x + 1][y] == '>' && !pos[x + 1][y + 1])
+                {
+                    // cout<<pos[x + 1][y + 1]<<" ";
+                    pos[x + 1][y + 1] = 1;
+                    qp.push({x + 1, y + 1});
+                }
+            }
+            if (x + 1 <= 1)
+            {
+                if (s[x + 1][y] == '<' && !pos[x + 1][y - 1])
+                {
+                    // cout<<pos[x + 1][y - 1]<<" ";
+                    pos[x + 1][y - 1] = 1;
+                    qp.push({x + 1, y - 1});
+                }
+            }
+            if (y - 1 >= 0)
+            {
+                if (s[x][y - 1] == '<' && !pos[x][y - 2])
+                {
+                    // cout<<pos[x][y - 2]<<" ";
+                    pos[x][y - 2] = 1;
+                    qp.push({x, y - 2});
+                }
+            }
+            if (y + 1 < n)
+            {
+                if (s[x][y + 1] == '>' && !pos[x][y + 2])
+                {
 
-   }
+                    // cout<<pos[x][y + 2]<<" ";
+                    pos[x][y + 2] = 1;
+                    qp.push({x, y + 2});
+                }
+            }
+
+            // cout<<endl;
+
+        }
+
+        // for(int i=0;i<2;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<pos[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        if (pos[1][n - 1]) cout << "YES\n";
+        else cout << "NO\n";
+
+    }
 }
 
 
+
+/*
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder or not inorder:
+           return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root. left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
+
+
+*/
--- test04.cpp
+++ test04.cpp
@@ -1,26 +1,85 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
-#define ll long long
-//JAI SHRI RAM
-int main() {
-    ll t, n, m, count, i, x, y;
-    cin >> t;
-    while( t-- )
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
     {
-        cin >> n >> m;
-        count = 0;
-        vector< ll > v(m);
-        for(i = 0; i < m; i++) {
-            cin >> v[i];
-            count += v[i];
+        if (preorder.empty() || inorder.empty())
+
+            return nullptr;
+        unordered_map<int, int> inorder_map;
+        unordered_map<int, int> inorder_map;
+        for (int i = 0; i < inorder.size(); ++i) {
+            inorder_map[inorder[i]] = i;
         }
-        x = count/n;
-        y = count%n;
-        if(x >= m) {
-            cout << n << endl;
-        } else if(x+1 == m) {
-            cout << y << endl;
-        } else cout << "0" << endl;
+        return build(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inorder_map);
     }
+
+    TreeNode *build(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, unordered_map<int, int> &inorder_map) {
+         if (preStart > preEnd || inStart > inEnd) {
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+         int mid = inorder_map[root->val];
+
+        root->left = build(preorder, preStart + 1, preStart + mid - inStart, inorder, inStart, mid - 1, inorder_map);
+
+        root->right = build(preorder, preStart + mid - inStart + 1, preEnd, inorder, mid + 1, inEnd, inorder_map);
+
+        return root;                    
+
+    }
+};
+
+int main()
+{
+    // Example input
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+
+    // Create an instance of Solution class
+    Solution solution;
+
+    // Build the binary tree
+    TreeNode *root = solution.buildTree(preorder, inorder);
+
+    // Print the root node value (just for demonstration)
+    cout << "Root node value: " << root->val << endl;
+
+    // Perform any other operations with the binary tree if needed
+
+    // Free memory (if not using smart pointers)
+    delete root;
+
     return 0;
 }
+
+/**
+ * Definition for a binary tree node.
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+  105. Construct Binary Tree from Preorder and Inorder Traversal
+
+  
+ */
