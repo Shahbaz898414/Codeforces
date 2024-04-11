69a80005a2229cac0f9137930e0aeafbb0cd2c58
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-11 14:17:03 +0530

    1

--- test04.cpp
+++ test04.cpp
@@ -4,115 +4,28 @@ using namespace std;
 
 #define ll long long
 
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-
-
-class Solution {
-public:
-    unordered_map<Node*,Node*> old;
-
-     Node* cloneGraph(Node* node) {
-       if(!node) return NULL;
-       return dfs(node); 
-    }
-
-    Node* dfs(Node* node){
-        if(old.find(node)!=old.end()){
-            return old[node];
-        }
-
-        Node* copy=new Node(node->val);
-        old[node]=copy;
-        for(Node* it:node->neighbors){
-            copy->neighbors.push_back(dfs(it));
-        }
-
-        return copy;
-    }
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main()
 {
 
-   int n;cin>>n;
+    ll t;cin>>t;
 
-   vector<int>  ar(n);
+    while(t--){
+        ll n,m,s;cin>>n>>m>>s;
+        vector<ll>  ar(m+1);
 
-   for (int i = 0; i < n; i++)
-   {
-    /* code */
-    cin>>ar[i];
-   }
+        for (ll i = 1; i <=m; i++) {
+            /* code */
+            cin>>ar[i];
+        }
+        
+    }
 
-//    cout<<longestConsecutive(ar)<<endl;
+ 
    
 
     return 0;
 }
 
 
-
-/*
-
-If it is truly meant for you, if will come
-back to you, it will leave only for the
-sake of teaching you the lessons you could
-only learn on your own. If it is truly meant
-for you, it will return even if you've puched it
-away, even if you are in denial, even if you
-assume Samething So beautiful could never be
-truly yours - because if it's truly meant for you,
-it is never not a piece of you. It is never not
-intricately tied into the depths of your soul.
-
-
-
-
-*/
