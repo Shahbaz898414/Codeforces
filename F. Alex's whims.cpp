8851aab86c854fe8e7c43e67e2b700a76a76a26d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-15 02:13:13 +0530

    1

--- F. Alex's whims.cpp
+++ F. Alex's whims.cpp
@@ -22,6 +22,26 @@ signed main()
 /*
 
 
+Look, it is not at all that a girl like
+Hinata cannot exist in our world
+but the girls of our world
+do not want to be like Hinata
 
+or have you ever seen a girl
+who loves a boy whom
+the whole world hates?
+
+The one who doesn't have a single friend
+is a bit raw in mind
+and is also financially unstable
+like Narutoo
+
+but girls like boys who are
+popular, handsome
+intelligent, fool and are also
+financially stable
+
+In your efforts to find a handsome person,
+They lost a good hearted person
 
 */
