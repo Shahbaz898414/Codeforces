293ab09b9c622136d3a1cdd8d847bdc3a21bd4c9
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-17 15:28:47 +0530

    1

--- F. Alex's whims.cpp
+++ F. Alex's whims.cpp
@@ -3,40 +3,50 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.length() + 1, false);
+        dp[s.length()] = true;
+
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            for (const string& word : dict) {
+                if (i + word.length() <= s.length() && s.substr(i, word.length()) == word) {
+                    dp[i] = dp[i + word.length()];
+                }
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
 
 
 signed main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int t;
-  cin >> t;
-  while (t--) {
-   int n,m;cin>>n>>m;
+  Solution sol;
 
+    // Example input
+    string s = "leetcode";
+    vector<string> wordDict = {"leet", "code"};
 
-  }
+    // Check if the string can be segmented
+    bool result = sol.wordBreak(s, wordDict);
+
+    // Output the result
+    cout << (result ? "True" : "False") << endl;
 }
 
-
-
-/*
-
-photolithography is the process that defines and
-transfers pattern onto a thin film layer on the
-wafer
-In the process a light soource is used to  transfer an
-image from a pattern mask to a photosensitive layer
-(photresist or resist) on a substrate or another thin film.
-
-Component for photolithography are photo resists mask
-and uv light.
-A positiye photo resist is a type of photo resist in which
-the portion of the photo resist that is exposed to light
-becomes soluble to the photo resist developer.
-A negate photo resist is a type of photresist in which
-the portion of the photresist that is expsed to light
-becomes insoluble to the photo resist developer
-
-
-*/
