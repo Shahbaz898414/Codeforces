413dadc679abcd14fc72048b693d74994273cfc4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-22 02:59:59 +0530

    1

--- test04.cpp
+++ test04.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -9,34 +10,71 @@ int main()
     cin >> t;
     while (t--)
     {
-        int num, k_val;
-        cin >> num >> k_val;
+        int n; cin >> n;
 
-        if (num == 1)
+
+        vector<int> nums(n);
+        for (int i = 0; i < n; ++i)
         {
-            cout << k_val << endl;
+            cin >> nums[i];
         }
-        else
+
+        vector<int> prefix(n),suff(n),v(n);
+        prefix[0] = nums[0];
+
+
+        for (int i = 1; i < n; ++i)
         {
-            int f = 0;
-            if (k_val + 1 == (1 << ((int)log2(k_val))))
-            {
-                cout << k_val << " ";
-            }
-            else
-            {
-                cout << (1 << (int)log2(k_val)) - 1 << " ";
-                f = k_val - ((1 << (int)log2(k_val)) - 1);
-            }
-
-            for (int i = 0; i < (num - 1); i++)
-            {
-                cout << f << " ";
-                f = 0;
-            }
-
-            cout << endl;
+            suff[i] = suff[i - 1] ^ suff[i];
         }
+        
+
+
+        for (int i = 1; i < n; ++i)
+        {
+            prefix[i] = prefix[i - 1] ^ nums[i];
+            v[i]=prefix[i]+suff[i];
+        }
+
+
+        int cnt = 0,ans=0,f=0;
+
+  
+        for (int a = 0; a < n; ++a) {
+            for (int b = a; b < n; ++b) {
+                for (int c = b; c < n; ++c) {
+
+                    int f1 = (a == 0) ? suff[b] : suff[b] ^ suff[a - 1];
+
+                    int f_ab = (a == 0) ? prefix[b] : prefix[b] ^ prefix[a - 1];
+                    
+                    int f_bc = (b == 0) ? prefix[c] : prefix[c] ^ prefix[b - 1];
+
+                    int f2 = (b == 0) ? suff[c] : suff[c] ^ suff[b - 1];
+                    int f_ac = (a == 0) ? prefix[c] : prefix[c] ^ prefix[a - 1];
+
+                    int f3 = (a == 0) ? suff[c] : suff[c] ^ suff[a - 1];
+
+                     if ((f1 ^ f2) > f3)
+                        ans++;
+
+                      
+                         int f4=(a == 0) ? v[c] : v[c] ^ v[a - 1];
+                        int f5=(b == 0) ? v[c] : v[c] ^ v[a - 1];
+                        int f6=(c == 0) ? v[c] : v[c] ^ v[a - 1];
+
+
+                    if ((f_ab ^ f_bc) > f_ac)
+                        cnt++,ans--;
+
+                    if ((f4 ^ f5) > f6)
+                        f=1;      
+                    
+                }
+            }
+        }
+
+        cout<<max(cnt,ans*f)<<endl;
     }
 
     return 0;
@@ -70,4 +108,8 @@ It is guaranteed that the sum of n over all test cases does not exceed 3 . 10^5.
 Output
 For each test case, output a single integer on a new line - the total number of possible final configurations modulo 10^9 + 7.
 
+
+//////////////
+
+ imports the Font Awesome stylesheet directly from a CDN.
 */
