bf8a358dbca8f113c6ebbd9cc3b2ffabc238ceca
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-25 12:58:34 +0530

    1

--- C. Nene's Magical Matrix.cpp
+++ C. Nene's Magical Matrix.cpp
@@ -22,3 +22,17 @@ int main()
 }
 
 
+
+
+/*
+
+
+Hi,
+
+I hope this message finds you well! I came across the Back End Developer intern (https://www.linkedin.com/jobs/view/3907293630/?trackingId=pX1PY9XkRYyVX3fOBXd4%2Bw%3D%3D&refId=vXXNAXxfStiQOpl8ww175A%3D%3D&midToken=AQE4d6DUV4se3w&midSig=1LGPEGJ_deUrc1&trk=eml-jobs_jymbii_digest-job_card-0-jobcard_body&trkEmail=eml-jobs_jymbii_digest-job_card-0-jobcard_body-null-ep2k1g~lvdlpja8~yh-null-null&eid=ep2k1g-lvdlpja8-yh&otpToken=MWEwZDE2ZTYxMTI4YzhjMGJjMjQwNGVkNDcxZGVlYjE4ZmNjZDg0MjljYWY4ZTYxNzljNjA3NmI0OTUzNWJmMGYwZDRkZjg2NmFmN2M4ZjI2YzgwZmY3ZGEwNWEzNzBmM2E3ZGY2MzEyNmI2ZTE5YzZkNjJkZCwxLDE%3D) at PW. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+
+Best regards,
+Shahbaz Khan
+
+
+*/
