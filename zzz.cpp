09623f71f1bca8e5dee159f4d906cbacf2b1ac13
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-04-29 20:07:53 +0530

    1

--- zzz.cpp
+++ zzz.cpp
@@ -1,90 +1,289 @@
-#include <bits/stdc++.h>
-#define int long long
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+#include <math.h>
+#include <stack>
+#include <list>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <utility>
+#define int int64_t
 #define vi vector<int>
+#define vii vector<pair<int, int>>
+#define vs vector<string>
+#define vc vector<char>
 #define vb vector<bool>
-#define readInput(a) \
-	for (auto &i : a)  \
-	cin >> i
-#define printOutput(a) \
-	for (auto &i : a)    \
-	cout << i << " "
-#define endLine "\n"
-#define space << " " <<
-#define pushBack pb
-#define allElements(a) a.begin(), a.end()
-#define reverseAll(a) a.rbegin(), a.rend()
-const int N = 1e6 + 2;
-const int MOD = 1e9 + 7;
-const int INF = LLONG_MAX;
+#define pb push_back
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define mp make_pair
+#define amax(a, b) a = max(a, b)
+#define amin(a, b) a = min(a, b)
+#define all(x) (x).begin(), (x).end()
+#define vin(x, v)   \
+  for (auto &x : v) \
+    cin >> x;
+#define vout(x, v) \
+  for (auto x : v) \
+    cout << x << " ";
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define loop(i, j, k) for (int i = j; i < k; i += 1)
+#define rloop(i, j, k) for (int i = j; i >= k; i -= 1)
+#define rep(i, j) loop(i, 0, j)
+#define rrep(i, j) rloop(i, j, 0)
+#define MP make_pair
+#define endl "\n"
+#define INF (int)1e18
+#define EPS 1e-18
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+// cout <<setprecision(15)
+#define NEED_FOR_SPEED_MOST_WANTED  \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL)
 using namespace std;
 
-int countSubarraysWithAtMostKDistinct(vector<int> &nums, int k)
+#ifndef ONLINE_JUDGE
+#define deb(x)       \
+  cerr << #x << " "; \
+  _print(x);         \
+  cerr << endl;
+#else
+#define deb(x)
+#endif
+
+void _print(int32_t t)
 {
-	int n = nums.size();
+  cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
 
-	unordered_map<int, int> mp;
-
-	int i = 0, j = 0;
-
-	int c = 0;
-
-	while (j < n)
-	{
-
-
-		mp[nums[j]]++;
-
-
-		while (i <= j && mp.size() > k)
-		{
-			if (--mp[nums[i]] == 0)
-				mp.erase(nums[i]);
-			i++;
-		}
-
-
-		c += (j - i + 1);
-
-
-		j++;
-
-
-	}
-
-	return c;
-
-
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+  cerr << "{";
+  _print(p.first);
+  cerr << ",";
+  _print(p.second);
+  cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+  cerr << "[ ";
+  for (T i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+  cerr << "[ ";
+  for (auto i : v)
+  {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
 }
 
-int subarraysWithKDistinct(vector<int> &nums, int k)
+vii graph[200010];
+map<int, int> degree;
+
+void dij(int C, vi &dist)
 {
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-	// int d1=countSubarraysWithAtMostKDistinct(nums, k);
-	// int d2=countSubarraysWithAtMostKDistinct(nums, k - 1);
+  dist[C] = 0;
+  pq.push({0, C});
+  while (pq.size() > 0)
+  {
+    pair<int, int> alpha = pq.top();
+    pq.pop();
+    if (dist[alpha.second] < alpha.first)
+      continue;
+    for (auto x : graph[alpha.second])
+    {
+      if (dist[x.first] > dist[alpha.second] + x.second)
+      {
+        dist[x.first] = dist[alpha.second] + x.second;
+        pq.push({dist[x.first], x.first});
+      }
+    }
+  }
+}
+const int N = 1e5 + 5;
+vector<int> adj[N];
+int val[N], xor_arr[N];
+int vis[(int)1e7 + 1];
+int cnt = 0;
 
-	// cout<<d1<<" "<<d2<<endl;
-	return countSubarraysWithAtMostKDistinct(nums, k) - countSubarraysWithAtMostKDistinct(nums, k - 1);
-	// return 1;
+int dfs1(int u)
+{
+  xor_arr[u] = val[u];
+  vis[u] = true;
+
+  for (auto v : adj[u])
+  {
+    if (!vis[v])
+    {
+      xor_arr[u] ^= dfs1(v);
+    }
+  }
+  return xor_arr[u];
+}
+
+int dfs2(int u)
+{
+  vis[u] = true;
+  int tmp = val[u];
+  for (auto v : adj[u])
+  {
+    if (!vis[v])
+    {
+      tmp ^= dfs2(v);
+    }
+  }
+
+  if (tmp == xor_arr[0])
+  {
+    tmp = 0;
+    // cout<<v<<"tree"<<endl;
+    cnt++;
+  }
+  return tmp;
+}
+
+void solve() {
+
+  int n;cin>>n;
+
+  vi a(n);
+
+  map<int,int> mp;
+
+  int mx=0;
+
+
+  for (int i = 0; i < n; i++) {
+    cin>>a[i];
+
+    mp[a[i]]++;
+
+   
+  }
+
+  for (int i = 0; i < n; i++) {
+    
+
+    mx=max(mx,mp[a[i]]);
+  }
+  
+
+  int cnt = 0, mxcnt = 0;
+    for (auto it : mp)
+    {
+        if (it.second == mx)
+        {
+            mxcnt++;
+        }
+    }
+
+    // cout<<mx<<" "<<mxcnt<<endl;
+
+
+    int rem = n - mxcnt * mx;
+
+    // cout<<rem<<" "<<(rem / (mx - 1))<<endl;
+
+
+    cout << (mxcnt - 1) + (rem / (mx - 1)) << endl;
+
+
+
 
 
 }
 
 signed main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n, k;
-	cin >> n >> k;
+  NEED_FOR_SPEED_MOST_WANTED;
 
-	vector<int> arr(n);
 
-	for (int i = 0; i < n; i++)
-	{
-		/* code */
-		cin >> arr[i];
-	}
+  // #ifndef ONLINE_JUDGE
+  // FOR GETTING INPUT FROM input.txt
+  // freopen("input.txt", "r", stdin);
+  // FOR GETTING INPUT FROM input.txt
+  // freopen("output.txt", "w", stdout);
+  //	#endif
 
-	// cout<<endl;
+  // for (int i = 1; i  <= 30; i++)
+  // {
+  //   cout << vis[i] << " ";
+  // }
 
-	cout << subarraysWithKDistinct(arr, k);
+  // cout << "shah " << endl;
+
+  int t = 1;
+
+  cin >> t;
+  while (t--)
+  {
+
+    int n;cin>>n;
+
+		vector<int>  arr(n);
+
+		for (int i = 0; i < n; i++) {
+			cin>>arr[i];
+		}
+		
+    
+  }
 }
