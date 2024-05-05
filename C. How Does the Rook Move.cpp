7b20bda47bf48dec22e58b85396101434b3c9b49
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-06 01:22:20 +0530

    1

---
+++ C. How Does the Rook Move.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define int long long
+#define vi vector<int>
+#define vb vector<bool>
+#define readInput(a) \
+  for (auto &i : a)  \
+  cin >> i
+#define printOutput(a) \
+  for (auto &i : a)    \
+  cout << i << " "
+#define endLine "\n"
+#define space << " " <<
+#define pushBack pb
+#define allElements(a) a.begin(), a.end()
+#define reverseAll(a) a.rbegin(), a.rend()
+const int N = 1e6 + 2;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX;
+using namespace std;
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+
+   
+
+   
+  }
+}
+
+
+
