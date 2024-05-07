f15512ba5b1c34f7fb1deea2dc2cfd38cd992bb4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-08 00:02:18 +0530

    1

--- C. How Does the Rook Move.cpp
+++ C. How Does the Rook Move.cpp
@@ -18,21 +18,58 @@ const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX;
 using namespace std;
 
-signed main()
+long long calculate(int n)
 {
+  long long dp[300001];
+  dp[0] = dp[1] = 1;
+  for (int i = 2; i <= n; i++)
+  {
+    dp[i] = (dp[i - 1] + 2LL * (i - 1) * dp[i - 2]) % MOD;
+  }
+  return dp[n];
+}
+
+
+
+signed main() {
+
+  
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   int t;
   cin >> t;
-
   while (t--)
   {
-
-   
-
-   
+    int n, k, x, y;
+    cin >> n >> k;
+    while (k--)
+    {
+      cin >> x >> y;
+      n -= 1 + (x != y);
+    }
+    cout << calculate(n) << endl;
   }
 }
 
 
 
+/*
+
+#include<iostream>
+using namespace std;
+typedef long long ll;
+ll n,k,x,y,i,d[300001];
+main(){
+
+
+ for(i=2,d[0]=d[1]=1;i<=300000;i++)
+  d[i]=(d[i-1]+2*(i-1)*d[i-2])%1000000007;
+
+
+ for(cin>>n;cin>>n>>k;cout<<d[n]<<endl)
+  while(k--)
+   cin>>x>>y,n-=1+(x!=y);
+}
+
+
+*/
