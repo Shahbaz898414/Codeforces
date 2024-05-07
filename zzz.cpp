d5b7ade378e4c2bf13b0652e921bb6993891b1cc
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-07 21:30:41 +0530

    1

--- zzz.cpp
+++ zzz.cpp
@@ -1,74 +1,144 @@
-#include <iostream>
-#include <vector>
 #include <unordered_map>
-#include <unordered_set>
+#include <string>
 
 using namespace std;
 
-class Solution {
+class TrieNode {
 public:
-    // Declare dfs as a member function of the class
-    bool dfs(int crs, unordered_map<int, vector<int>>& preMap, unordered_set<int>& visitSet) {
-        if (visitSet.count(crs)) {
-            return false;
-        }
-        if (preMap[crs].empty()) {
-            return true;
-        }
+    unordered_map<char, TrieNode*> children;
+    bool word;
 
-        visitSet.insert(crs);
-        for (int pre : preMap[crs]) {
-            if (!dfs(pre, preMap, visitSet)) { // Call dfs recursively
-                return false;
-            }
-        }
-        visitSet.erase(crs);
-        preMap[crs].clear();
-        return true;
+    TrieNode() {
+        word = false;
+    }
+};
+
+class WordDictionary {
+public:
+    TrieNode* root;
+
+    WordDictionary() {
+        root = new TrieNode();
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // Map each course to its prerequisite list
-        unordered_map<int, vector<int>> preMap;
-        for (const auto& pre : prerequisites) {
-            preMap[pre[0]].push_back(pre[1]);
-        }
-
-        // Set to track all courses along the current DFS path
-        unordered_set<int> visitSet;
-
-        // Check if each course can be finished
-        for (int crs = 0; crs < numCourses; ++crs) {
-            if (!dfs(crs, preMap, visitSet)) { // Call dfs function
-                return false;
+    void addWord(string word) {
+        TrieNode* cur = root;
+        for (char c : word) {
+            if (cur->children.find(c) == cur->children.end()) {
+                cur->children[c] = new TrieNode();
             }
+            cur = cur->children[c];
         }
-        return true;
+        cur->word = true;
+    }
+
+    bool search(string word) {
+        function<bool(int, TrieNode*)> dfs = [&](int j, TrieNode* root) {
+            TrieNode* cur = root;
+
+            for (int i = j; i < word.length(); ++i) {
+                char c = word[i];
+
+                if (c == '.') {
+                    for (auto& [ch, child] : cur->children) {
+                        if (dfs(i + 1, child)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    if (cur->children.find(c) == cur->children.end()) {
+                        return false;
+                    }
+                    cur = cur->children[c];
+                }
+            }
+            return cur->word;
+        };
+
+        return dfs(0, root);
     }
 };
 
 int main() {
-    // Input
-    int numCourses;
-    cin >> numCourses;
-    
-    int numPrerequisites;
-    cin >> numPrerequisites;
+    WordDictionary* obj = new WordDictionary();
+    obj->addWord("bad");
+    obj->addWord("dad");
+    obj->addWord("mad");
 
-    vector<vector<int>> prerequisites(numPrerequisites, vector<int>(2));
-    for (int i = 0; i < numPrerequisites; ++i) {
-        cin >> prerequisites[i][0] >> prerequisites[i][1];
-    }
-
-    // Create an instance of the Solution class
-    Solution sol;
-
-    // Output
-    if (sol.canFinish(numCourses, prerequisites)) {
-        cout << "Possible to finish all courses." << endl;
-    } else {
-        cout << "Impossible to finish all courses." << endl;
-    }
+    bool search_1 = obj->search("pad"); // Output: false
+    bool search_2 = obj->search("bad"); // Output: true
+    bool search_3 = obj->search(".ad"); // Output: true
+    bool search_4 = obj->search("b.."); // Output: true
 
     return 0;
 }
+
+
+/*
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or 
+                r == ROWS or c == COLS or
+                (r, c) in visit or board[r][c] not in node.children):
+                return
+
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+
+            dfs(r - 1, c, node, word)
+            dfs(r + 1, c, node, word)
+            dfs(r, c - 1, node, word)
+            dfs(r, c + 1, node, word)
+            visit.remove((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+
+        return list(res)
+
+
+
+212. Word Search ll Tree blind 75,Blind 75,Blind 75 LeetCode Sheet Solved,Blind 75 Sheet Solved,Word Search ll Tree blind 75,Word Search ll Tree,212. Word Search ll Tree,212 Word Search ll Tree on leetcode,212 on leetcode,212 on leetcode solved,leetcode,Word Search ll Tree c++,Word Search ll Tree python,Word Search ll Tree solution
+
+
+#75dayschallenge
+#challenge
+#consistency
+#Cp 
+#AlgorithmExplained 
+#ProblemSolving
+#lessons
+#learning
+#Youtube
+#video
+#explaination
+
+*/
