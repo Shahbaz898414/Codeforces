57433e7d57dfc771860c26bb166c8d904bcb2621
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-11 15:27:13 +0530

    1

--- zzz.cpp
+++ zzz.cpp
@@ -1,47 +1,48 @@
+#include <string>
 #include <vector>
+#include <bits/stdc++.h>
 #include <algorithm>
+using namespace std;
 
 class Solution {
 public:
-    static bool cmp(const std::pair<int, int>& a, const std::pair<int, int>& b){
-        return a.second < b.second;
-    }
-    
-    int eraseOverlapIntervals(std::vector<std::vector<int>>& intervals) {
-        if (intervals.empty()) {
-            return 0;
-        }
-        
-        int n = intervals.size();
-        std::vector<std::pair<int, int>> v(n);
-        
-        for(int i = 0; i < n; i++) {
-            v[i].first = intervals[i][0];
-            v[i].second = intervals[i][1];
-        }
-        
-        std::sort(v.begin(), v.end(), cmp);
+    int longestCommonSubsequence(std::string t1, std::string t2) {
+        int m = t1.length();
+        int n = t2.length();
 
-        int end = v[0].second;
-        int count = 0; 
+        // Create a matrix to store the lengths of common subsequences
+        std::vector<std::vector<int>> matrix(m + 1, std::vector<int>(n + 1, 0));
+      
 
-        for(int i = 1; i < n; i++) {
-            if(v[i].first < end) {
-                count++;
-            } else {
-                end = v[i].second;
+        // Fill the matrix bottom-up
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (t1[i] == t2[j]) {
+                    matrix[i][j] = 1 + matrix[i + 1][j + 1];
+                } else {
+                    matrix[i][j] = std::max(matrix[i][j + 1], matrix[i + 1][j]);
+                }
+                
             }
+            
         }
+
         
-        return count;
+        
+
+        // Return the length of the longest common subsequence
+        return matrix[0][0];
     }
 };
 
 int main() {
-    Solution sol;
-    std::vector<std::vector<int>> intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
-    int result = sol.eraseOverlapIntervals(intervals);
-    std::cout << "Result: " << result << std::endl;
+    // Test case
+    Solution solution;
+    std::string t1 = "abcde";
+    std::string t2 = "ace";
+    int result = solution.longestCommonSubsequence(t1, t2);
+    cout << "Length of longest common subsequence: " << result << endl;
+
     return 0;
 }
 
@@ -54,52 +55,17 @@ int main() {
 /*
 
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+    def longestCommonSubsequence(self, t1: str, t2: str) -> int:
+        matrix =[[0 for j in range(len(t2)+1)] for i in range(len(t1)+1)]
 
-        def dfs(r, c, visit, prevHeight):
-            if ((r, c) in visit or
-            r < 0 or c < 0 or r == ROWS or c == COLS or
-            heights[r][c] < prevHeight):
-                return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+        for i in range(len(t1)-1, -1, -1):
+            for j in range(len(t2)-1, -1, -1):
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[r][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+            if t1[i] == t2[j]:
+            matrix[i][j] =1 + matrix[i+1][j+1]
+            else:
+            matrix[i][j] = max(matrix[i][j+1],matrix[i+1][j])
+        return matrix[0][0]
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][c])
-            dfs(r, COLS - 1, atl, heights[r][c])
-        
-        res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res. append([r, c])
-
-        return res
-
-
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        hm = {}
-        ans = 0
-
-        l= 0
-        for r in range(len(s)):
-            hm[s[r]] = 1 + hm.get(s[r]],0)
-
-            whibe (r-1+1) - max(hm.values()) > k:
-                hm[s[1]] -= 1
-                l += 1
-
-            ans = max(ans, r-1+1)
-        return ans
 
 */
