4a946d5646298f576ee686e3eff592573688e125
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-14 06:15:37 +0530

    1

--- zzz.cpp
+++ zzz.cpp
@@ -98,4 +98,69 @@ Co-ordinate frames
 #learning
 #Youtube
 
+
+////////////////////
+Q. 6 Write short notes on any two :-
+(a) Generalized Voronoi Diagram (GVD)
+(b) Kinematic parameters
+(c) Robotic work cell
+(d) Shrink & Swell Operators
+
+Normal sliding and approach vector
+
+///////////////////////////////////////////////
+
+The Denavit - Hartenberg (D-H RepresentaHon.
+(D-H Mlgtorithm gives sysiematic hataHon for assighing
+Yighi- handed orthonormal - Co-ardinate frames ,one to each link
+in an open Kinemdlic. Chain af links)(Atler assighing Co-asdinok
+frames, transfor mation berween adjacent co-ordinate frames
+Can ten be vepresented by a single stendard 4x4
+homogeneous co-ardinate hansfor mation mdtrix)
+frhe D-H algorithm is a two pass algorithm . on the
+.ftrst pass C steps o through 7), a ser of ntl vight handed
+erthonoxmal co-ordinate frames assigned to each unk(on
+The Second pass (steps 8 thsougn 13), the values for the.
+KinemaHc. paxameters (0, d,a) x) are deler mined ?
+
+Number the Joints from It n Starhng Wiw me
+ending wih the too). yaw, pitch and voll in mel- ordey ..
+Asslgn a vighr handed orthonssmal co-erdinate frame La to
+the robot base, making Sure that z"aligns with the axis
+of joint 1. set K=l.
+Allgn zk with the axis of jolnt k+ !.
+Locale the orlgin af Lk at the IntersecHon of the zh and
+zk axes . If they do not Intersect, use the Intessechon af
+zk with a common novmal between. zkand zki.
+Selech xk to be ovthogonal to both zh and zk4. 2f zk
+and zk-i are parallel, point xk away from.k !.
+Selecr yk ts form a right handed ortno normal Ca-odinase frame.
+LK.
+Ser K=ktl. f k<n, ga + Step a; ele cantinue.
+ser the origin af Ln at the tool Hp . Aign z" with the
+appooach vecheryh with the Sliding vecies, and xn with hhe.
+horma Vector of te tool. ser kel.
+
+//////////////////////////////////
+
+27
+
+General propestles of SolyHions - (pI .. Reler Class Notes))
+1- Gxistence.of Solutions -
+if.the desived tool -Hp position p is ourside. its Wask envelape
+then no SoluHon can exist.
+Even. When p .is withln the Work envelope, there may be
+certain tool onenratons R Which are not realixable
+Without violating one or more of the joint variable limils.
+
+2 _ Uniqueness of Solution -
+The existence of a soluHon to the inverse kinematics
+Problem Is not the only Issue dhat needs to be addressed.
+When soluHons clo exist, typically they are not unique.
+Foy example, So me tobots are. designed wimn naxes
+Wheve h>6. Fo these Ychots, inhnitely muny solutiont
+to the Inverse kinematice problem tpically exist. Hence.
+h>6 robots are rejerred as KinemaHcally redundant
+robots, because. Wey have more degrees of freedom
+han are necessary to estahli.sh arbitary tool Confiqtralh.
 */
