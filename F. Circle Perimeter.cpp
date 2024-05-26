66ab4102ec4a09bcbd9134411964adf8cf877794
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-26 23:35:19 +0530

    1

--- F. Circle Perimeter.cpp
+++ F. Circle Perimeter.cpp
@@ -21,6 +21,21 @@ int main()
 			while(i*i+y2*y2<(n+1)*(n+1))y2++;
 			ans+=(y2-y1);
 		}
+    
 		cout<<ans*4<<endl;
 	}
 }
+
+/*
+
+
+Hi,
+
+I hope this message finds you well! I came across the Software Engineer ( https://www.deshawindia.com/careers/all-positions-in-software-development-2614 ) at D. E. Shaw & Co. As an aspiring Software Engineer, I admire the company's innovative work. If possible, could you kindly refer me? I'd greatly appreciate it!
+
+Best regards,
+Shahbaz Khan
+
+
+
+*/
