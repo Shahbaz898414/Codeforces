6a67e5dc545abcdadcfe53e2017c27e7e48b2bb5
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-05-28 19:09:37 +0530

    1

--- F. Circle Perimeter.cpp
+++ F. Circle Perimeter.cpp
@@ -28,55 +28,31 @@ int main()
 
 /*
 
-A Wireless ad hoc network is a group of independent terminals or nodes which communicate with each other by forming a multi-hop radio network.
 
-Nodes in this networks have to compete with
-some effects of radio communication such as
-interference, noise and fading etc. as the
-communication between the nodes takes place
-over the wireless (radio) links.
+Iptables is a Linux command line firewall which
+allows system administrators to manage incoming
+and outgoing traffic through a set of configurable
+table rules.
 
-- The links in wireless ad hoc network have less
-bandwidth as compared to that of a wired
-network.
+IPTable is a tool for managing firewall rules on a
+Linux OS machine. IPTable is a firewall program for
+Linux OS.
 
-- In wireless ad hoc networks or simply an ad hoc
-network, each node acts as a router and a host
-and the network control is distributed among the
-nodes.
+It monitors traffic from and to your server by using
+tables.
 
-Infrastructure-less network is a group of self-
-configurable, autonomous, self-organizing nodes
-connected by wireless links.
-- The nodes can move freely from one to another
-place by changing the topology regularly because
-there is no predefined infrastructure. In this type
-of networks, communication between nodes takes
-place using multi-hop communication.
+Iptables is used to set up, maintain and inspect the
+tables of IP packet filter rules in the Linux kernel
 
-- In Ad hoc network, the network formation takes
-place dynamically through the collaboration of an
-random set of independent nodes.
-- Due to decentralized network, topology discovery
-and delivery of message should be executed by
-nodes themselves.
+These tables includes a set of rules, called as
+chains. The chains will filter incoming and
+outgoing data packets.
 
-
-The nodes can join or leave the network at any
-time due to free node mobility.
-
-- Wireless ad hoc networks allows short range and
-long range communication ranging from wireless
-mobile networks to wireless sensor networks.
-
-- In wireless Ad hoc network, the network topology
-is highly dynamic due to mobility of nodes.
-
-- The nodes in ad hoc network can communicate
-with each other directly within their wireless range
-whereas they can communicate with each other by
-using multi-hop radio network beyond their
-wireless range.
-
+Iptables is a command-line firewall service that
+uses policy chains to allow or block the traffic.
+When a connection tries to set up itself on your
+system, iptables looks for a rule in its list in order
+to match it to. If it doesn't matches, it takes the
+default action.
 
 */
