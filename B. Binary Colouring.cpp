567b1e061d97250df12eb916bfc1b3979528cacd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-06-02 00:50:17 +0530

    1

--- B. Binary Colouring.cpp
+++ B. Binary Colouring.cpp
@@ -3,38 +3,50 @@ using namespace std;
 
 #define ll long long
 
+int main()
+{
+  ll t;
+  cin >> t;
 
-
-
-
-int main() {
-    ll t;cin>>t;
-
-    while(t--){
-      ll n;cin>>n;
-      vector<ll> arr;
-
-      
+  while (t--)
+  {
+    ll x;
+    cin >> x;
+    vector<int> res(31, 0);
+    for (int i = 0; i < 30; i++)
+    {
+      if (1ll & (x >> i))
+      {
+        if (res[i])
+        {
+          res[i + 1] = 1;
+          res[i] = 0;
+        }
+        else if (i > 0)
+        {
+          if (res[i - 1] == 1)
+          {
+            res[i + 1] = 1;
+            res[i - 1] = -1;
+          }
+          else
+          {
+            res[i] = 1;
+          }
+        }
+        else if (i == 0)
+        {
+          res[i] = 1;
+        }
+      }
     }
+    cout << 31 << '\n';
+    for (int i = 0; i <= 30; i++)
+    {
+      cout << res[i] << ' ';
+    }
+    cout << '\n';
+  }
 
-    return 0;
+  return 0;
 }
-
-/*
-
-
-5kg tel
-10kg aata
-1/4 jira
-1kg tuvar dal
-1/2kg chala
-1/2kg vatana
-1kg chana dal
-100gm haldi
-
-1.5kg saraf
-chura and biscuit
-1/2kg masur ki dal
-
-
-*/
