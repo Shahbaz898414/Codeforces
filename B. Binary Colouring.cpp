c1db413148071475d9c9eebd388540d26f2042e2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-06-01 23:01:25 +0530

    1

--- B. Binary Colouring.cpp
+++ B. Binary Colouring.cpp
@@ -20,3 +20,21 @@ int main() {
     return 0;
 }
 
+/*
+
+
+5kg tel
+10kg aata
+1/4 jira
+1kg tuvar dal
+1/2kg chala
+1/2kg vatana
+1kg chana dal
+100gm haldi
+
+1.5kg saraf
+chura and biscuit
+1/2kg masur ki dal
+
+
+*/
