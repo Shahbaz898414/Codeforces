fd339fd6d19485ad3a0ffcde70fd722f507eb6b6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-06-18 19:46:33 +0530

    1

--- test6.cpp
+++ test6.cpp
@@ -36,6 +36,43 @@ void rotateMatrix(vector<vector<int>> &v, int n) {
   }
 }
 
+ 
+
+ class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        if(list1 == NULL){
+            return list2;
+        }
+
+        if(list2 == NULL){
+            return list1;
+        }
+
+        if(list1 -> val > list2 -> val){
+            swap(list1,list2);
+        }
+        
+
+        ListNode* res = list1; 
+        
+         
+        while(list1 != NULL && list2 != NULL){
+            ListNode* temp = NULL;
+            while(list1 != NULL && list1 -> val <= list2 -> val){
+                temp = list1;
+                list1 = list1 -> next;
+            }
+            temp -> next = list2;   
+            swap(list1,list2);
+        }
+
+
+        return res;
+    }
+};
+
 set<int> mp;
 void thr(vector<vector<int>> &v, int n) {
   for(int i = 0; i < n / 2; i++){
@@ -107,3 +144,32 @@ int main() {
 
     return 0;
 }
+
+
+/*
+
+ std::string encode(std::vector<std::string>& strs) {
+        std::string ans = "";
+        for (const std::string& s : strs) {
+            ans += std::to_string(s.length()) + "$" + s;
+        }
+        return ans;
+    }
+
+    std::vector<std::string> decode(std::string s) {
+        std::vector<std::string> ans;
+        int i = 0;
+        while (i < s.length()) {
+            int j = i;
+            while (s[j] != '$') {
+                j++;
+            }
+            int len = std::stoi(s.substr(i, j - i));
+            ans.push_back(s.substr(j + 1, len));
+            i = j + 1 + len;
+        }
+        return ans;
+    }
+
+
+*/
