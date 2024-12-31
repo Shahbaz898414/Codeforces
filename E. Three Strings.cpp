562fd5439febc8063cf4ac01d26ffeeb6fe3053b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2024-12-31 22:26:50 +0530

    31-12-2024

--- E. Three Strings.cpp
+++ E. Three Strings.cpp
@@ -134,29 +134,23 @@ int32_t main()
   cin >> t;
   while (t--)
   {
+    string a, b, c;
+    cin >> a >> b >> c;
 
-    int n, k;
-    cin >> n;
-
-    int arr[n];
-
-    if (n == 1)
-    {
-      cout << 1 << endl;
-    }
-    else if (n == 2)
-    {
-      cout << 2 << endl;
-    }
-    else
-    {
-    }
+    cout << a << " " << b << " " << c << endl;
   }
   return 0;
 }
 
 /*
+
 You are given three strings: a , b , and c , consisting of lowercase Latin letters.
-The string c  was obtained in the following way:  At each step, either string a  or string b  was randomly chosen, and the first character of the chosen string was removed from it and appended to the end of string c , until one of the strings ran out. After that, the remaining characters of the non-empty string were added to the end of c . Then, a certain number of characters in string c  were randomly changed. For example, from the strings a=abra  and b=cada , without character replacements, the strings caabdraa , abracada , acadabra  could be obtained.  Find the minimum number of characters that could have been changed in string c .  Input The first line of the input contains a single integer t  (1≤t≤103 ) — the number of test cases.  The first line of each test case contains one string of lowercase Latin letters a  (1≤|a|≤103 ) — the first string, where |a|  denotes the length of string a .  The second line of each test case contains one string of lowercase Latin letters b  (1≤|b|≤103 ) — the second string, where |b|  denotes the length of string b .  The third line of each test case contains one string of lowercase Latin letters c  (|c|=|a|+|b| ) — the third string.  It is guaranteed that the sum of |a|  across all test cases does not exceed 2⋅103 . Also, the sum of |b|  across all test cases does not exceed 2⋅103 .  Output For each test case, output a single integer — the minimum number of characters that could have been changed in string c .
+The string c  was obtained in the following way:
+At each step, either string a  or string b  was randomly chosen, and the first character of the chosen string was removed from it and appended to the end of string c , until one of the strings ran out. After that, the remaining characters of the non-empty string were added to the end of c . Then, a certain number of characters in string c  were randomly changed. For example, from the strings a=abra  and b=cada , without character replacements, the strings caabdraa , abracada , acadabra  could be obtained.  Find the minimum number of characters that could have been changed in string c.
+
+
+Input The first line of the input contains a single integer t  (1≤t≤103 ) — the number of test cases.  The first line of each test case contains one string of lowercase Latin letters a  (1≤|a|≤103 ) — the first string, where |a|  denotes the length of string a .  The second line of each test case contains one string of lowercase Latin letters b  (1≤|b|≤103 ) — the second string, where |b|  denotes the length of string b .  The third line of each test case contains one string of lowercase Latin letters c  (|c|=|a|+|b| ) — the third string.  It is guaranteed that the sum of |a|  across all test cases does not exceed 2⋅103 . Also, the sum of |b|  across all test cases does not exceed 2⋅103 .
+
+Output For each test case, output a single integer — the minimum number of characters that could have been changed in string c .
 
 */
